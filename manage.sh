8e09621eb9fd95149c9827c97274cb703e4f7e38
marc wimmer <marc@itewimmer.de>
2016-06-28 19:42:36 +0200

    ..

--- manage.sh
+++ manage.sh
@@ -104,7 +104,7 @@ init)
     ;;
 
 setup-startup)
-    file=/etc/init/${CUSTOMS}_odoo
+    file=/etc/init/${CUSTOMS}_odoo.conf
     echo "Setting up startup script in $file"
     cp $DIR/config/upstart $file
     sed -i -e "s/\${DCPREFIX}/$DCPREFIX/" -e "s/\${DCPREFIX}/$DCPREFIX/" $file
