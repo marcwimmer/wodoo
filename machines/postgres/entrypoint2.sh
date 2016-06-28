cd8a729aaec73021c840b007e1457c128ad9a1fe
root <marc@itewimmer.de>
2016-06-28 12:31:37 +0200

    simple entrypoint postgres

--- machines/postgres/entrypoint2.sh
+++ machines/postgres/entrypoint2.sh
@@ -10,37 +10,25 @@ if [[ -z "$PGDATA" ]]; then
     echo "Please define pgdata"
     exit -1
 fi
-if [[ -n "$DO_INIT" ]]; then
-    echo 'Initialising postgres'
-    rm $PGDATA/* -Rf
+
+if [[ -n "$DO_BACKUP" ]]; then
     /docker-entrypoint.sh postgres &
-    echo "Shutting down in 10 seconds..."
     sleep 10
-    psql template1 -c "SELECT datname FROM pg_database WHERE datistemplate = false;"
+    pg_dump $DBNAME|gzip > /opt/dumps/$DBNAME_$(date).gz
     pkill postgres
-    sleep 2
-    exit 0
 else
-    if [[ -n "$DO_BACKUP" ]]; then
+    if [[ -n "$RESTORE_DUMP" ]]; then 
+        echo "Restoring database $DBNAME"
         /docker-entrypoint.sh postgres &
-        sleep 10
-        pg_dump $DBNAME|gzip > /opt/dumps/$DBNAME_$(date).gz
+        sleep 5
+        dropdb $DBNAME || echo 'database did not exist'
+        createdb $DBNAME
+        gunzip -c /opt/dumps/$DBNAME.gz |psql $DBNAME
+        psql template1 -c "alter database $DBNAME owner to odoo;"
+        echo "Restoring snapshot done!"
         pkill postgres
     else
-        if [[ -n "$RESTORE_DUMP" ]]; then 
-            echo "Restoring database $DBNAME"
-            /docker-entrypoint.sh postgres &
-            sleep 5
-            dropdb $DBNAME || echo 'database did not exist'
-            createdb $DBNAME
-            gunzip -c /opt/dumps/$DBNAME.gz |psql $DBNAME
-            psql template1 -c "alter database $DBNAME owner to odoo;"
-            echo "Restoring snapshot done!"
-            pkill postgres
-        else
-            echo 'Normal postgres start...'
-            /docker-entrypoint.sh postgres
-        fi
+        echo 'Normal postgres start...'
+        /docker-entrypoint.sh postgres
     fi
 fi
-
