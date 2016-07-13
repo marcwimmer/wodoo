fddd0a7c8723165193a773dad199d165d56c1229
marc wimmer <marc@itewimmer.de>
2016-07-13 23:21:55 +0200

    ...

--- machines/odoo/install_python_packages.sh
+++ machines/odoo/install_python_packages.sh
@@ -4,15 +4,15 @@ set -x
 
 echo "Installing custom requirements from odoo"
 
+pip install -r /root/requirements.txt
+
 #7.0
 if [[ "$1" == "7.0" ]]; then
     echo "Installing version 7.0 requirements"
     pip install -r /root/requirements_70.txt
-    pip install -r /root/requirements.txt
 else
     echo "Installing version $1 requirements"
     wget https://raw.githubusercontent.com/odoo/odoo/$1/requirements.txt -O /root/requirements_$1.txt
     pip install -r /root/requirements_$1.txt
-    pip install -r /root/requirements.txt
 fi
 
