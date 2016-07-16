6f043fe3133426a4b53c05fb6b15dba3134b09d5
marc wimmer <marc@itewimmer.de>
2016-07-16 08:55:27 +0200

    data dir

--- machines/odoo/run_autosetup.sh
+++ machines/odoo/run_autosetup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Autosetup searches in /opt/openerp/customs/$CUSTOMS for 
+# Autosetup searches in /opt/openerp/customs/$CUSTOMS/autosetup for 
 # *.sh files; makes them executable and executes them
 # You can do setup there, like deploying ssh keys and so on
 set -e
