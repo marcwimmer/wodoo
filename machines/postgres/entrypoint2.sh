bb9ea8a161d761448386bf66e8745ed40c60db2a
root <marc@itewimmer.de>
2016-07-21 11:41:21 +0200

    can also handle gzip file

--- machines/postgres/entrypoint2.sh
+++ machines/postgres/entrypoint2.sh
@@ -24,7 +24,9 @@ else
         sleep 5
         dropdb $DBNAME || echo 'database did not exist'
         createdb $DBNAME
-        pg_restore -d $DBNAME /opt/dumps/$DBNAME.gz
+        pg_restore -d $DBNAME /opt/dumps/$DBNAME.gz || {
+            gunzip -c /opt/dumps/$DBNAME.gz | psql $DBNAME
+        }
         psql template1 -c "alter database $DBNAME owner to odoo;"
         echo "Restoring snapshot done!"
         pkill -f postgres
