b5ef42b68cbb4cdde8d6f164ba6db577872def49
marc wimmer <marc@itewimmer.de>
2016-09-01 14:38:47 +0200

    ...

--- machines/openvpn/make_certs.sh
+++ machines/openvpn/make_certs.sh
@@ -12,7 +12,7 @@ docker-compose run ovpnca /root/tools/make_client_keys.sh CLIENT
 
 # pack client scripts together
 docker-compose run ovpn-server /root/tools/pack_server_conf.sh
-docker-compose run ovpn-server /root/tools/pack_client_conf.sh asterisk asterisk.conf tar asterisk.tar
+docker-compose run ovpn-server /root/tools/pack_client_conf.sh asterisk asterisk.conf notar asterisk.conf
 docker-compose run ovpn-server /root/tools/pack_client_conf.sh CLIENT vpn.cnf tar snom.tar
 docker-compose run ovpn-server /root/tools/pack_client_conf.sh CLIENT softphone.conf notar softphone.conf 
 
