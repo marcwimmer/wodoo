7483d6052da95235ad3cad1a0774cfcc8f16aa5e
marc@itewimmer.de <marc@itewimmer.de>
2016-09-26 08:14:45 +0200

    M  config/docker-compose.yml.tmpl M  customs.env.template M  machines/odoo/install_python_packages.sh

--- machines/odoo/install_python_packages.sh
+++ machines/odoo/install_python_packages.sh
@@ -10,6 +10,9 @@ pip install -r /root/requirements.txt
 if [[ "$1" == "7.0" ]]; then
     echo "Installing version 7.0 requirements"
     pip install -r /root/requirements_70.txt
+elif [[ "$1" == "6.1" ]]; then
+    echo "Installing version 6.1 requirements - but nothing set"
+    #pip install -r /root/requirements_70.txt
 else
     echo "Installing version $1 requirements"
     wget https://raw.githubusercontent.com/odoo/odoo/$1/requirements.txt -O /root/requirements_$1.txt
