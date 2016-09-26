2837470ee6232a5f20d1fcaa9cf6f8c79206acfa
marc@itewimmer.de <marc@itewimmer.de>
2016-09-26 10:16:30 +0200

    M  machines/odoo/install_python_packages.sh

--- machines/odoo/install_python_packages.sh
+++ machines/odoo/install_python_packages.sh
@@ -12,7 +12,8 @@ if [[ "$1" == "7.0" ]]; then
     pip install -r /root/requirements_70.txt
 elif [[ "$1" == "6.1" ]]; then
     echo "Installing version 6.1 requirements - but nothing set"
-    #pip install -r /root/requirements_70.txt
+    # use same requirements...
+    pip install -r /root/requirements_70.txt
 else
     echo "Installing version $1 requirements"
     wget https://raw.githubusercontent.com/odoo/odoo/$1/requirements.txt -O /root/requirements_$1.txt
