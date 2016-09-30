ad5536c707a46c353d2136436e70ff9dcc53d93e
marc wimmer <marc@itewimmer.de>
2016-09-30 15:58:05 +0200

    M  config/docker-compose.yml.tmpl M  machines/odoo/requirements.txt M  machines/odoo/run_autosetup.sh

--- machines/odoo/run_autosetup.sh
+++ machines/odoo/run_autosetup.sh
@@ -4,6 +4,8 @@
 # You can do setup there, like deploying ssh keys and so on
 set -e
 
+ODOO_PROD="$1"
+
 cd /opt/openerp/customs/$CUSTOMS
 if [[ ! -d autosetup ]]; then
     exit 0
@@ -12,5 +14,5 @@ cd autosetup
 
 for file in *.sh; do
     echo "executing $file"
-    eval "bash ./$file"
+    eval "bash ./$file $ODOO_PROD"
 done
