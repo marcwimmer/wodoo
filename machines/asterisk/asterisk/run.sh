8d214b631cc9b4cf3907d06769920db632b0d634
marc wimmer <marc@itewimmer.de>
2016-09-30 21:36:38 +0200

    ...

--- machines/asterisk/asterisk/run.sh
+++ machines/asterisk/asterisk/run.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -e
 
-# if not then you can debug on ari the stasis
-if [[ "$RUN_STASIS_ON_ASTERISK" == "1" ]]; then
-    # clone latest stasis application from deploy branck
-    [[ -d /opt/asterisk_ari ]] && rm -Rf /opt/asterisk_ari
-    git clone git.mt-software.de:/git/openerp/modules/asterisk_ari --branch deploy --single-branch /opt/asterisk_ari
-    cd /opt/asterisk_ari
-    git checkout deploy -f
-fi
-
 #connect to openvpn server
 mkdir /dev/net
 mknod /dev/net/tun c 10 200  # also used for tap
