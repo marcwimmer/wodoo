1d6d9302fd3a43df816f5f2096cc09ecef6494df
marc wimmer <marc@itewimmer.de>
2016-10-21 15:22:41 +0200

    M  machines/asterisk/ari/Dockerfile A  machines/asterisk/ari/debug.sh

---
+++ machines/asterisk/ari/debug.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+set -x
+
+cd /opt/src
+
+pip install pudb
+/usr/sbin/nginx
+
+echo "Starting ARI Connector...."
+cd /opt/src/asterisk_ari/connector
+python ariconnector.py \
+    --odoo-host $ODOO_HOST \
+    --odoo-port $ODOO_PORT \
+    --odoo-user $ODOO_USER \
+    --odoo-password $ODOO_PASSWORD \
+    --odoo-db $ODOO_DB
