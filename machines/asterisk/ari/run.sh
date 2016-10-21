c8bde0b5347ea27d9c6e7dbc08952c025ad9634d
marc wimmer <marc@itewimmer.de>
2016-10-21 15:33:38 +0200

    ...

--- machines/asterisk/ari/run.sh
+++ machines/asterisk/ari/run.sh
@@ -10,7 +10,7 @@ fi
 cd /opt/src
 
 # always updating
-rm /opt/src/asterisk_ari -Rf || true
+rm /opt/src/* -Rf || true
 git clone --branch deploy git.mt-software.de:/git/openerp/modules/asterisk_ari
 echo 'done updating ari'
 
