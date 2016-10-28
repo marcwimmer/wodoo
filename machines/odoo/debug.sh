4868a289a160cdfbbd97fa643eac6af195b84e1a
marc wimmer <marc@itewimmer.de>
2016-10-28 19:55:44 +0200

    ..

--- machines/odoo/debug.sh
+++ machines/odoo/debug.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 touch /tmp/debugging
+
+cd /tmp
+VIM=marcvim_installer_ubuntu-16.04.sh
+if [[ ! -f $VIM ]]; then
+    wget http://vim.itewimmer.de/$VIM
+    chmod a+x $VIM
+    ./$VIM
+fi
+pkill -9 -f vimupdate
+
+
 pkill -9 -f openerp || true
 sudo -H -u odoo /opt/openerp/versions/server/openerp-server -d $DBNAME --log-level=error
