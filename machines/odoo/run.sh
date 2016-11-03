c0ae65dc9920cf61cd9fa26d30731b7806349c0b
CPADMIN <cpadmin@erp>
2016-11-03 10:09:40 +0100

    ...

--- machines/odoo/run.sh
+++ machines/odoo/run.sh
@@ -127,7 +127,7 @@ if [[ "$ODOO_VERSION" == "6.1" ]]; then
 fi
 
 echo "Starting up odoo"
-sudo -H -u odoo /opt/openerp/versions/server/openerp-server -c /home/odoo/config_openerp -d $DBNAME --log-level=debug &
+#sudo -H -u odoo /opt/openerp/versions/server/openerp-server -c /home/odoo/config_openerp -d $DBNAME --log-level=debug &
 sudo -H -u odoo /opt/openerp/versions/server/openerp-gevent -c /home/odoo/config_gevent  -d $DBNAME --log-level=debug &
 sleep 3
 
