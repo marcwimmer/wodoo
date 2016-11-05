f26e0678b7ebcd38410cb2dbd7ab3c940c8914d7
marc wimmer <marc@itewimmer.de>
2016-11-05 06:42:38 +0100

    ...

--- machines/asterisk/ari/debug.sh
+++ machines/asterisk/ari/debug.sh
@@ -9,13 +9,7 @@ pkill -9 -f ariconnector
 echo "Starting ARI Connector...."
 cd /opt/src/asterisk_ari/connector
 
-python ariconnector.py \
-    --reset_locks \
-    --odoo-host $ODOO_HOST \
-    --odoo-port $ODOO_PORT \
-    --odoo-user $ODOO_USER \
-    --odoo-password $ODOO_PASSWORD \
-    --odoo-db $ODOO_DB
+python reset_locks.py
 python ariconnector.py \
     --debug \
     --odoo-host $ODOO_HOST \
--- machines/asterisk/ari/runprod.sh
+++ machines/asterisk/ari/runprod.sh
@@ -5,13 +5,7 @@ pkill -9 -f ari || true
 cd /opt/src/asterisk_ari/connector
 rm /tmp/starting || true
 
-python ariconnector.py \
-    --reset_locks \
-    --odoo-host $ODOO_HOST \
-    --odoo-port $ODOO_PORT \
-    --odoo-user $ODOO_USER \
-    --odoo-password $ODOO_PASSWORD \
-    --odoo-db $ODOO_DB
+python reset_locks.py
 
 python ariconnector.py \
     --odoo-host $ODOO_HOST \
