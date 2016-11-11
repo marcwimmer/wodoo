d5caeff30abd0bd0b956ac0f73ee03fa0127ca1d
marc wimmer <marc@itewimmer.de>
2016-11-11 13:18:18 +0100

    M  machines/odoo/debug.sh

--- machines/odoo/debug.sh
+++ machines/odoo/debug.sh
@@ -2,16 +2,5 @@
 touch /tmp/debugging
 
 # install marcvim
-cd /tmp
-VIM=marcvim_installer_ubuntu-16.04.sh
-if [[ ! -f $VIM ]]; then
-    apt-get install -y libruby wget
-    wget http://vim.itewimmer.de/$VIM
-    chmod a+x $VIM
-    ./$VIM
-fi
-pkill -9 -f vimupdate
-
-
 pkill -9 -f openerp || true
-sudo -H -u odoo /opt/openerp/versions/server/openerp-server -d $DBNAME --log-level=error
+sudo -H -u odoo /opt/openerp/versions/server/openerp-gevent -d $DBNAME --log-level=error
