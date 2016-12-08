f690a39475f2886cef6e95ea94e16ce08b7b82f7
root <you@example.com>
2016-12-08 10:34:50 +0100

    ..

--- machines/odoo/bin/update_modules.sh
+++ machines/odoo/bin/update_modules.sh
@@ -3,6 +3,7 @@ source /env.sh
 
 if [[ -n "$MODULE" ]]; then
     echo "updating just $MODULE"
+    MODULE=$(/opt/openerp/admin/update_custom_modules.py)
 else
     echo "Getting list of custom modules"
 fi
