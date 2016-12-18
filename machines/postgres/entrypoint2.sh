32685fa2912cda34321eb680cc1e7462192dc91a
root <you@example.com>
2016-12-18 14:19:08 +0100

    ...

--- machines/postgres/entrypoint2.sh
+++ machines/postgres/entrypoint2.sh
@@ -6,13 +6,17 @@ cd /
 echo "Executing postgres entrypoint2.sh"
 echo "DBNAME: $DBNAME"
 
-cat /postgres.conf > $PGDATA/postgresql.onf
-
-
 if [[ -z "$PGDATA" ]]; then
     echo "Please define pgdata"
     exit -1
 fi
+
+FILE=postgres2.conf
+grep -q $PGDATA/postgresql.conf "$CONFPATH" || {
+	cat "include_dir '$CONFPATH'" > $PGDATA/postgresql.conf
+}
+
+
 
 echo 'Normal postgres start...'
 /docker-entrypoint.sh postgres
