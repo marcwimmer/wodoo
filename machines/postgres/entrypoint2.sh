7ca0b8c8ec804abf269cc3f30dcb348f3a3ecbef
marc wimmer <marc@itewimmer.de>
2016-12-18 14:00:02 +0100

    added connection option

--- machines/postgres/entrypoint2.sh
+++ machines/postgres/entrypoint2.sh
@@ -6,6 +6,8 @@ cd /
 echo "Executing postgres entrypoint2.sh"
 echo "DBNAME: $DBNAME"
 
+cat /postgres.conf > $PGDATA/postgreslc.onf
+
 
 if [[ -z "$PGDATA" ]]; then
     echo "Please define pgdata"
