db7dd2ef8e93e74154694e53a3cdb22125fa97be
marc wimmer <marc@itewimmer.de>
2016-12-19 15:37:24 +0100

    ..

--- machines/cups/start_cups.sh
+++ machines/cups/start_cups.sh
@@ -10,6 +10,6 @@ if [ $(grep -ci $CUPS_USER_ADMIN /etc/shadow) -eq 0 ]; then
     useradd $CUPS_USER_ADMIN --system -G root,lpadmin --no-create-home --password $(mkpasswd $CUPS_USER_PASSWORD)
 fi
 
-python /print.py $WATCHPATH &
+sleep 10 && python /print.py $WATCHPATH &
 
 exec /usr/sbin/cupsd -f
