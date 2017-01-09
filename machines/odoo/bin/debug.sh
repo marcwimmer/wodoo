a1be6a5d69a5caea298af47990b9f9a8ef8e77e7
marc wimmer <marc@itewimmer.de>
2017-01-09 16:58:57 +0100

    ...

--- machines/odoo/bin/debug.sh
+++ machines/odoo/bin/debug.sh
@@ -2,5 +2,6 @@
 touch /tmp/debugging
 
 # install marcvim
+pip install pudb
 pkill -9 -f openerp || true
 sudo -E -H -u odoo /opt/openerp/versions/server/openerp-server -d $DBNAME -c /home/odoo/config_debug
