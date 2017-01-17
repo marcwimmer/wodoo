18d1aad685ebc443bd9b822a296358908c10ca28
marc wimmer <marc@itewimmer.de>
2017-01-17 11:01:44 +0100

    RUN_CRONJOBS

--- machines/odoo/bin/run.sh
+++ machines/odoo/bin/run.sh
@@ -37,7 +37,9 @@ if [[ "$ODOO_VERSION" == "6.1" ]]; then
 fi
 
 echo "Starting up odoo"
-sudo -E -H -u odoo /opt/openerp/versions/server/openerp-server -c /home/odoo/config_openerp -d $DBNAME --log-level=error &
+if [[ "$RUN_CRONJOBS" == "1" ]]; then
+    sudo -E -H -u odoo /opt/openerp/versions/server/openerp-server -c /home/odoo/config_openerp -d $DBNAME --log-level=error &
+fi
 sudo -E -H -u odoo /opt/openerp/versions/server/openerp-gevent -c /home/odoo/config_gevent  -d $DBNAME --log-level=error &
 sleep 3
 
