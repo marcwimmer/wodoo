c7322c744f85d59ceb9d0b8838e6b030d7ca8883
zotac.box <zotac.box>
2017-01-20 08:30:38 +0100

    better client generation

--- config/ovpn/pack.sh
+++ config/ovpn/pack.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 if [[ -z "$dc" ]]; then
     echo "docker-compose instruction missing"
     exit -1
@@ -8,7 +9,7 @@ fi
 runca="$dc run ovpn_ca"
 runmakekeys="$dc run ovpn_makekeys"
 
-# create client keys (correspnds with name in ccd)
+# create client keys (corresponds with name in ccd)
 $runca /root/tools/make_client_keys.sh asterisk
 $runca /root/tools/make_client_keys.sh dns
 $runca /root/tools/make_client_keys.sh ntp
@@ -16,19 +17,16 @@ $runca /root/tools/make_client_keys.sh odoo
 $runca /root/tools/make_client_keys.sh client
 $runca /root/tools/make_client_keys.sh server-as-client
 
-if [[ -n "$CUSTOM_VPN_CLIENTS" ]]; then
-    IFS=',' read -ra ARR <<< "$CUSTOM_VPN_CLIENTS"
-    for i in "${ARR[@]}"; do
-        $runca /root/tools/make_client_keys.sh "$i"
-    done
-fi
-
 # create routed clients(phones)
 if [[ -n "$PHONE_VPN_START" && -n "$PHONE_VPN_END" ]]; then
     i=$((PHONE_VPN_START))
     while $i < $((PHONE_VPN_END)); do
-        $runca /root/tools/make_client_keys.sh client-with-route$i
+        CLIENT=client-$i
+        $runca /root/tools/make_client_keys.sh "$CLIENT"
+        IP=$(python "$DIR/get_ip.py" "10.28.0.0" $i)
         i=$((i + 1))
+        # enter IP
+        sed -i "s|__IP__|$IP|g" "$DIR/ccd/$CLIENT"
     done
 fi
 
