17f5185c936040fa9444f4e889682aff7d907c0a
marc wimmer <marc@itewimmer.de>
2017-02-07 07:20:45 +0100

    fix

--- machines/odoo/bin/shell.sh
+++ machines/odoo/bin/shell.sh
@@ -2,4 +2,5 @@
 
 rsync /opt/src/admin/ /opt/openerp/admin -arP --delete --exclude=.git
 rsync /opt/src/customs/$CUSTOMS/ /opt/openerp/active_customs -arP --delete --exclude=.git
+/opt/openerp/admin/oeln $CUSTOMS
 sudo -E -H -u odoo /opt/openerp/versions/server/odoo.py shell -d $DBNAME -c /home/odoo/config_debug
