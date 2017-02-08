ff47c43bd81535d76cae9cd9351a94e19feb85e9
marc wimmer <marc@itewimmer.de>
2017-02-08 22:09:26 +0100

    M  machines/odoo/bin/rootrun.sh

--- machines/odoo/bin/rootrun.sh
+++ machines/odoo/bin/rootrun.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 
-if [[ -z "$CUSTOMS" ]]; then
-    echo "CUSTOMS required!"
-    exit -1
-fi
-
 if [[ "$IS_ODOO_CRONJOB" == "1" && "$RUN_CRONJOBS" != "1" ]]; then
     echo "Cronjobs shall not run. Good-bye!"
     exit 0
