6201c50ea7d33661fed22e86c5d15e4fa4c4b6f3
marc wimmer <marc@itewimmer.de>
2017-02-15 18:19:12 +0100

    fixed rights

--- machines/odoo/bin/shell.sh
+++ machines/odoo/bin/shell.sh
@@ -3,4 +3,5 @@
 rsync /opt/src/admin/ /opt/openerp/admin -arP --delete --exclude=.git
 rsync /opt/src/customs/$CUSTOMS/ /opt/openerp/active_customs -arP --exclude=.git
 /opt/openerp/admin/oeln $CUSTOMS
+chown odoo:odoo /opt/openerp/active_customs -R
 sudo -E -H -u odoo /opt/openerp/versions/server/odoo.py shell -d $DBNAME -c /home/odoo/config_debug
