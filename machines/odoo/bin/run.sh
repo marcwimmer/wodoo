4d7bde8e38bd8476336d769c3c47042594605911
marc <marc@itewimmer.de>
2017-08-06 14:59:02 +0200

    added check for set_trace's before start

--- machines/odoo/bin/run.sh
+++ machines/odoo/bin/run.sh
@@ -10,6 +10,12 @@ echo "Executing autosetup..."
 /run_autosetup.sh
 echo "Done autosetup"
 
+# abort start, if traces are found
+cd /opt/openerp/active_customs
+/opt/openerp/admin/find-traces && {
+    echo "traces found - start aborted"
+    exit 1
+}
 
 echo "Starting up odoo"
 if [[ "$IS_ODOO_CRONJOB" == "1" ]]; then
