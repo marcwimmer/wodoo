442b674872d1958d97f6dc75ef49b6f75c4f7052
marc <marc@itewimmer.de>
2017-08-15 20:34:05 +0200

    restructured ari/stasis pythons and nginx

--- machines/asterisk/flask_nginx_proxy/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -ex
-[[ -z "$HOST" ]] && { echo 'HOST missing'; exit -1; }; 
-[[ -z "$PORT" ]] && { echo 'PORT missing'; exit -1; };
-sed -i "s/__HOST__/$HOST/g" /etc/nginx/sites-available/nginx.conf 
-sed -i "s/__PORT__/$PORT/g" /etc/nginx/sites-available/nginx.conf
-/usr/sbin/nginx -g 'daemon off;'
--- machines/asterisk/stasis/sysroot/usr/local/bin/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-cd /opt/src
-echo "Starting Stasis stasis.py"
-python stasis.py
