c3ff86aa96cfd169de9e6e95b47dc9e9756654a7
marc <marc@itewimmer.de>
2017-08-20 20:25:21 +0200

    fixes in keygenerator

--- machines/openvpn/openvpn/bin/run_client_key_generator_web.py
+++ machines/openvpn/openvpn/bin/run_client_key_generator_web.py
@@ -5,6 +5,8 @@ import os
 import subprocess
 import base64
 import process_config
+import tempfile
+from datetime import datetime
 
 urls = (
     '/', 'index',
@@ -45,18 +47,14 @@ class keygenerator:
         data = web.input(
             client_name="",
             conf_template="",
-            conf_filename="",
             tar='0'
         )
         if not data.client_name:
             raise Exception("Please provide query-string client_name")
 
-        conf_filename = data.conf_filename
-        tar = data.tar == '1'
-        if tar and not conf_filename.endswith(".tar"):
-            conf_filename += '.tar'
+        tempfilename = "generated_by_web_".format(datetime.now().strftime("%Y-%m-%d%H%M%S.conf"))
 
-        assert '/' not in conf_filename
+        tar = data.tar == '1'
 
         if web.ctx['path'] == '/generate':
             subprocess.check_output([
@@ -68,12 +66,12 @@ class keygenerator:
             '/usr/local/bin/pack_client_conf.sh',
             data.client_name,
             data.conf_template,
-            conf_filename,
+            tempfilename,
             '-tar' if tar else '',
         ])
 
         try:
-            dest_file_path = os.path.join(os.environ['CLIENT_OUT'], conf_filename)
+            dest_file_path = os.path.join(os.environ['CLIENT_OUT'], tempfilename)
 
             with open(dest_file_path, 'r') as f:
                 content = f.read()
