39928e0bc23fc78d5d5f31568efb6bb450f73794
marc <marc@itewimmer.de>
2017-08-25 20:57:00 +0200

    M  machines/odoo/bin/unit_test.sh

--- machines/odoo/bin/unit_test.sh
+++ machines/odoo/bin/unit_test.sh
@@ -8,14 +8,11 @@ fi
 reset
 /apply-env-to-config.sh
 
-cd $ADMIN_DIR/module_tools
-module=$(python -c "import module_tools; print module_tools.get_module_of_file('$1')")
 path=$ADDONS_CUSTOMS/$module/tests/$(basename $1)
 
 sudo -E -H -u $ODOO_USER $SERVER_DIR/openerp-server \
     -d $DBNAME \
     -c $CONFIG_DIR/config_unittest \
-	-u $module \
     --pidfile=$DEBUGGER_ODOO_PID \
     --stop-after-init \
     --test-file=$path \
