47305790799ac58c7171ee08e96a1df5e1a7162b
Marc Wimmer <marc@itewimmer.de>
2017-09-27 22:24:38 +0200

    just echo

--- machines/nginx/bin/run.sh
+++ machines/nginx/bin/run.sh
@@ -5,5 +5,7 @@ echo 'setting up nginx conf'
 cp /mysite.template /etc/nginx/conf.d/default.conf 
 sed -i "s/__HTTP_HOST__/$(echo "$HTTP_HOST" | tr ',' ' ')/g" /etc/nginx/conf.d/default.conf
 sed -i "s/__PATH_DIR__/default/g" /etc/nginx/conf.d/default.conf
+
+echo "Listing content of /etc/nginx/paths:"
 ls -lha /etc/nginx/paths
 nginx -g 'daemon off;'
