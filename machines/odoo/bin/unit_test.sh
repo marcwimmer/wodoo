fb4daa2e8e041e525294b0e60e354ffc94201508
Marc Wimmer <marc@itewimmer.de>
2017-11-03 07:50:08 +0100

    check script unittest

--- machines/odoo/bin/unit_test.sh
+++ machines/odoo/bin/unit_test.sh
@@ -8,15 +8,15 @@ fi
 reset
 /apply-env-to-config.sh
 
-cd $ADMIN_DIR/module_tools
+cd "$ADMIN_DIR/module_tools" || exit -1
 module=$(python -c "import module_tools; print module_tools.get_module_of_file('$1')")
-path=$ADDONS_CUSTOMS/$module/tests/$(basename $1)
+path="$ADDONS_CUSTOMS/$module/tests/$(basename "$1")"
 
-sudo -E -H -u $ODOO_USER $SERVER_DIR/openerp-server \
-    -d $DBNAME \
-    -c $CONFIG_DIR/config_unittest \
-    --pidfile=$DEBUGGER_ODOO_PID \
+sudo -E -H -u "$ODOO_USER" "$SERVER_DIR/openerp-server" \
+    -d "$DBNAME" \
+    -c "$CONFIG_DIR/config_unittest" \
+    --pidfile="$DEBUGGER_ODOO_PID" \
     --stop-after-init \
-    --test-file=$path \
+    --test-file="$path" \
     --test-report-directory=/tmp \
     --log-level=debug
