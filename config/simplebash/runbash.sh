e432cbe8f231d7bff137c8a0af83914fcf323872
Marc Wimmer <marc@itewimmer.de>
2017-11-20 08:10:19 +0100

    fixes in versioning

--- config/simplebash/runbash.sh
+++ config/simplebash/runbash.sh
@@ -4,7 +4,10 @@ export DIR="$DIR"
 uuid="$(uuidgen)"
 HOSTNAME_TO_USE="host_${uuid}"
 export HOSTNAME_TO_USE="$HOSTNAME_TO_USE"
-xhost +local:"$HOSTNAME_TO_USE"
+
+if [[ -n "$DISPLAY" ]]; then
+	xhost +local:"$HOSTNAME_TO_USE"
+fi
 docker-compose run \
 	--name "odoo_bash_$uuid" \
 	-e HOST_ODOO_HOME="$DIR" \
