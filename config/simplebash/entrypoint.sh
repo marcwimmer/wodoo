5360114ce21f6f6fe161dbd5c9fd346f862b8d08
Marc Wimmer <odoo>
2017-12-06 19:46:04 +0100

    queryfornulluser

--- config/simplebash/entrypoint.sh
+++ config/simplebash/entrypoint.sh
@@ -2,7 +2,7 @@
 set +x
 set -e
 
-if [[ "$USER" != "root" ]]; then
+if [[ "$USER" != "root" && "$USER" != "" ]]; then
 	useradd --uid "$UID" --home-dir /opt/external_home "$USER"
 	groupadd -g "$DOCKER_GROUP_ID" docker  # to be able to access docker socket
 	usermod -aG docker "$USER"
