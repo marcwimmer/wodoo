2b5bfab7575c44a5349df72028eb0d3e72ccd72b
Marc Wimmer <odoo>
2018-01-02 00:26:36 +0100

    renamed davical --> calendar

--- machines/calendar/config.php
+++ machines/calendar/config.php
@@ -1,5 +1,5 @@
 <?php
   $c->default_locale = "de_DE";
-  $c->pg_connect[] = 'host=davical_db user=davical password=davical dbname=davical';
+  $c->pg_connect[] = 'host=calendar_db user=davical password=davical dbname=davical';
   $c->dbg = array( 'statistics' => 1, 'request' => 1, 'response' => 1 );
 ?>
