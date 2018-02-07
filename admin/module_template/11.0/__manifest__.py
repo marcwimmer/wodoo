456dddad61401e9a255fb7d81f7b87647d758bb5
Marc Wimmer <odoo>
2018-02-07 10:57:59 +0100

    mod template cleaned

--- admin/module_template/11.0/__manifest__.py
+++ admin/module_template/11.0/__manifest__.py
@@ -2,17 +2,10 @@
 {
     'auto_install': False,
     'name': '__module_name__',
-    'description': '',
     'version': '1.0',
     'author': 'Marc Wimmer (marc@itewimmer.de)',
-    'category': 'uncategorized',
     'depends': ['base'],
     'demo_xml': [],
     'data': [],
-    'test': [],
     'installable': True,
-    'web': False,
-    'js': [],
-    'css': [],
-    'qweb': [],
 }
