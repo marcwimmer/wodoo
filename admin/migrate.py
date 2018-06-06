be3a383fd6b7a0498aebfff4f94b54acfa600944
Marc Wimmer <odoo>
2018-06-06 13:33:04 +0200

    .

---
+++ admin/migrate.py
@@ -0,0 +1,125 @@
+"""
+
+Migration Script
+
+
+"""
+import os
+import sys
+import logging
+import subprocess
+from logging import FileHandler
+from optparse import OptionParser
+from ConfigParser import SafeConfigParser
+from module_tools.odoo_config import customs_dir
+
+config = SafeConfigParser()
+parser = OptionParser(
+    description='Migration from odoo x to odoo y'
+    ''
+    '-log is written to stdout and parallel to $CUSTOMS_FOLDER/migration.log'
+    '-put a migration folder into your customs'
+    '-put destination .version into your customs'
+)
+parser.add_option(
+    "-L", "--log-file", action="store", type="string",
+    dest="log_file",
+    help="log file (required)")
+parser.add_option(
+    "-F", "--from-version", action="store", type="string",
+    dest="from_version",
+    help="migrate from version (required)")
+parser.add_option(
+    "-T", "--to-version", action="store", type="string",
+    dest="to_version",
+    help="migrate to version (required)")
+parser.add_option(
+    "-C", "--manage-command", action="store", type="string",
+    dest="manage_command",
+    help="manage command default /opt/odoo/admin/odoo-admin (required)")
+(options, args) = parser.parse_args()
+
+
+if not options.from_version or \
+        not options.to_version or \
+        not options.manage_command or \
+        not options.log_file:
+    parser.print_help()
+    sys.exit()
+
+config.read(options.from_version)
+
+FORMAT = '[%(levelname)s] %(name) -12s %(asctime)s %(message)s'
+logging.basicConfig(filename="/dev/null", format=FORMAT)
+logging.getLogger().setLevel(logging.DEBUG)
+logger = logging.getLogger('')  # root handler
+formatter = logging.Formatter(FORMAT)
+
+if os.path.exists(options.log_file):
+    os.unlink(options.log_file)
+rh = FileHandler(filename=options.log_file)
+rh.setFormatter(formatter)
+rh.setLevel(logging.DEBUG)
+logger.addHandler(rh)
+
+migrations = {
+    '11.0': {
+        'server': {
+            'url': 'git://github.com/OpenUpgrade/OpenUpgrade.git',
+            'branch': '11.0',
+            'cmd': 'odoo-bin --update=all --database=%(db)s '
+                   '--config=%(config)s --stop-after-init --no-xmlrpc',
+        },
+    },
+    '10.0': {
+        'server': {
+            'url': 'git://github.com/OpenUpgrade/OpenUpgrade.git',
+            'branch': '10.0',
+            'addons_dir': os.path.join('odoo', 'addons'),
+            'root_dir': os.path.join(''),
+            'cmd': 'odoo-bin --update=all --database=%(db)s '
+                   '--config=%(config)s --stop-after-init --no-xmlrpc',
+        },
+    },
+    '9.0': {
+        'server': {
+            'url': 'git://github.com/OpenUpgrade/OpenUpgrade.git',
+            'branch': '9.0',
+            'cmd': 'openerp-server --update=all --database=%(db)s '
+                   '--config=%(config)s --stop-after-init --no-xmlrpc',
+        },
+    },
+    '8.0': {
+        'server': {
+            'url': 'git://github.com/OpenUpgrade/OpenUpgrade.git',
+            'branch': '8.0',
+            'cmd': 'openerp-server --update=all --database=%(db)s '
+                   '--config=%(config)s --stop-after-init --no-xmlrpc',
+        },
+    },
+    '7.0': {
+        'server': {
+            'url': 'git://github.com/OpenUpgrade/OpenUpgrade.git',
+            'branch': '7.0',
+            'cmd': 'openerp-server --update=all --database=%(db)s '
+                   '--config=%(config)s --stop-after-init --no-xmlrpc '
+                   '--no-netrpc',
+        },
+    },
+}
+
+if options.from_version not in migrations:
+    print "Invalid from version: {}".format(options.from_version)
+
+
+for version in sorted(filter(lambda v: float(v) >= float(options.from_version) and float(v) <= float(options.to_version), migrations), key=lambda x: float(x)):
+    with open(os.path.join(customs_dir(), '.version'), 'w') as f:
+        f.write(version)
+    logger.info("""\n========================================================================
+Migration to Version {}
+========================================================================""".format(version)
+                )
+    #subprocess.check_call([options.manage_command, "build"])
+
+with open(os.path.join(customs_dir(), '.version'), 'w') as f:
+    f.write(options.to_version)
--- admin/prepare_dockercompose_files.py
+++ admin/prepare_dockercompose_files.py
@@ -8,6 +8,7 @@ from yaml import load, dump
 from datetime import datetime
 import subprocess
 import inspect
+from docker_tools import get_run_command
 local_odoo_home = os.environ['LOCAL_ODOO_HOME']
 host_odoo_home = os.environ["ODOO_HOME"]
 
@@ -131,34 +132,10 @@ def post_process_complete_yaml_config(yml):
 
     return yml
 
-def get_docker_image():
-    hostname = os.environ['HOSTNAME']
-    result = [x for x in subprocess.check_output(["/opt/docker/docker", "inspect", hostname]).split("\n") if "\"Image\"" in x]
-    if result:
-        result = result[0].split("sha256:")[-1].split('"')[0]
-        return result[:12]
-    raise Exception("Image not determined")
-
 
 # call docker compose config to get the complete config
 files = sorted(temp_files, key=lambda x: float(x.split("/")[-1].replace("-", ".")))
-cmdline = []
-cmdline.append("/opt/docker/docker")
-cmdline.append("run")
-cmdline.append('-e')
-cmdline.append('ODOO_HOME={}'.format(host_odoo_home))
-for envfile in ['settings', 'settings.override']:
-    envfile = os.path.join(local_odoo_home, envfile)
-    if os.path.exists(envfile):
-        cmdline.append('--env-file')
-        cmdline.append(envfile)
-cmdline.append("--rm")
-cmdline.append('-v')
-cmdline.append("{HOST_ODOO_HOME}:{HOST_ODOO_HOME}".format(HOST_ODOO_HOME=os.environ["ODOO_HOME"]))
-cmdline.append("--workdir")
-cmdline.append(host_odoo_home)
-cmdline.append(get_docker_image())
-
+cmdline = get_run_command()
 cmdline.append("/usr/local/bin/docker-compose")
 for file in files:
     cmdline.append('-f')
