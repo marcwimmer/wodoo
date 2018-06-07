d5000118a10838f7cd76eb7c9980014b13972c19
Marc Wimmer <odoo>
2018-06-08 01:15:39 +0200

    ...

--- machines/odoo/bin/backup_files.sh
+++ machines/odoo/bin/backup_files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 [[ "$VERBOSE" == "1" ]] && set -x
 
-DESTFILE=/opt/dumps/odoofiles.tar
-tar cfz $DESTFILE /opt/files
+DESTFILE="/opt/dumps/$1"
+tar cfz "$DESTFILE" /opt/files
 if [[ -n "$LINUX_OWNER_CREATED_BACKUP_FILES" ]]; then
-	chown $LINUX_OWNER_CREATED_BACKUP_FILES $DESTFILE
+	chown "$LINUX_OWNER_CREATED_BACKUP_FILES" "$DESTFILE"
 fi
--- machines/odoo/bin/restore_files.sh
+++ machines/odoo/bin/restore_files.sh
@@ -2,10 +2,10 @@
 [[ "$VERBOSE" == "1" ]] && set -x
 set -e
 
-if [[ -z "$RESTOREFILE" ]]; then
-	echo "RESTOREFILE is missing."
+if [[ -z "$1" ]]; then
+	echo "File is missing."
 	exit -1
 fi
 
 /set_permissions.sh
-tar vxfz "$RESTOREFILE" -C /
+tar vxfz "/opt/dumps/$1" -C /
