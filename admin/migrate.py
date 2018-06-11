c359d95191113f1870926f79040ef1c418c43a4e
root <marc@itewimmer.de>
2018-06-11 14:30:39 +0200

    ...

--- admin/migrate.py
+++ admin/migrate.py
@@ -120,6 +120,8 @@ def do_migrate(log_file, from_version, to_version, do_command, SETTINGS_D_FILE, 
     ========================================================================""".format(version)
                     )
 
+        from pudb import set_trace
+        set_trace()
         do_command('build')
         # make sure postgres is available
         do_command("wait_for_container_postgres")
