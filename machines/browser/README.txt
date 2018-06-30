43e07d78774c05da3c5e034ee54d5bd2e0e95048
Marc Wimmer <marc@itewimmer.de>
2018-06-30 23:21:47 +0200

    M  machines/browser/Dockerfile A  machines/browser/README.txt M  machines/browser/cmd.sh M  machines/browser/docker-compose.yml A  machines/browser/fluxbox/keys A  machines/browser/fluxbox/menu D  machines/browser/passwd D  machines/browser/vncconfig R  machines/browser/xstartup -> machines/browser/vncconfig/xstartup

---
+++ machines/browser/README.txt
@@ -0,0 +1,1 @@
+password is odoo2000
--- machines/browser/cmd.sh
+++ machines/browser/cmd.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 chown root /root/.vnc -R
+
+echo "$PASSWORD" | vncpasswd -f > /root/.vnc/passwd
 chmod o-rwx,g-rwx /root/.vnc/passwd
 vncserver :1 -geometry 1440x960 -depth 24
 while true;
