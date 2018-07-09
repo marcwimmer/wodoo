2a885e9d3a12e073f76183ef8e526269ac1ed9f8
Marc Wimmer <marc@itewimmer.de>
2018-07-09 11:45:16 +0200

    M  machines/odoo/bin/rootrun.sh M  machines/odoo/default.settings

--- machines/odoo/bin/rootrun.sh
+++ machines/odoo/bin/rootrun.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 [[ "$VERBOSE" == "1" ]] && set -x
 
-if [[ "$IS_ODOO_CRONJOB" == "1" && "$RUN_CRONJOBS" != "1" ]]; then
+if [[ "$IS_ODOO_CRONJOB" == "1" && "$RUN_ODOO_CRONJOBS" != "1" ]]; then
     echo "Cronjobs shall not run. Good-bye!"
     exit 0
 fi
