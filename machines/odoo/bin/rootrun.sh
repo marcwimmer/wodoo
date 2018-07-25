3a588b3012732d5c15453727e6dfe3889864f6e3
zotac.box <zotac.box>
2018-07-25 07:52:00 +0200

    .

--- machines/odoo/bin/rootrun.sh
+++ machines/odoo/bin/rootrun.sh
@@ -3,6 +3,9 @@ set -e
 set -x
 [[ "$VERBOSE" == "1" ]] && set -x
 
+echo "IS_ODOO_CRONJOB: $IS_ODOO_CRONJOB"
+echo "RUN_ODOO_CRONJOBS: $RUN_ODOO_CRONJOBS"
+
 if [[ "$IS_ODOO_CRONJOB" == "1" && "$RUN_ODOO_CRONJOBS" != "1" ]]; then
     echo "Cronjobs shall not run. Good-bye!"
     exit 0
