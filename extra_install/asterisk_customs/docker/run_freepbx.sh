bdfa097832d2feb7bb5f7ce961fbfa4b121b9649
Marc Wimmer <marc@itewimmer.de>
2018-08-02 10:49:05 +0200

    M  run_freepbx.sh

--- extra_install/asterisk_customs/docker/run_freepbx.sh
+++ extra_install/asterisk_customs/docker/run_freepbx.sh
@@ -13,3 +13,4 @@ docker-compose run --rm --service-ports -e EXTERNIP=$1 freepbx
 echo "Now goto http://localhost:9080 and setup username password."
 echo "Please create PJSIP extensions, not SIP"
 echo "Using hostname $EXTERNIP"
+echo "Now you should start ./debug_connector_mqtt_asterisk and debug the application"
