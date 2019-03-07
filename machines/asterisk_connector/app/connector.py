d11b9b24de165ba9d607a2b285341793d16065cc
Marc Wimmer <marc@itewimmer.de>
2019-03-07 17:52:43 +0100

    .

--- machines/asterisk_connector/app/connector.py
+++ machines/asterisk_connector/app/connector.py
@@ -165,9 +165,6 @@ class Connector(object):
                 redisStrict.srem('DND', x)
 
     def _get_channel(self, id):
-        if id and str(id).startswith('b'):
-            from pudb import set_trace
-            set_trace()
         redisStrict = redis.StrictRedis(connection_pool=redis_connection_pool)
         result = redisStrict.get('channel,{}'.format(id))
         if result:
