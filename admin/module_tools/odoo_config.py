b353b0a9a718d7ab7e6a9d7b68494da704bda2e7
marc <marc@itewimmer.de>
2019-04-03 04:38:40 -0500

    .

--- admin/module_tools/odoo_config.py
+++ admin/module_tools/odoo_config.py
@@ -28,7 +28,7 @@ def admin_dir():
 def customs_root():
     if os.getenv("DOCKER_MACHINE", "0") == "1":
         if os.getenv("ACTIVE_CUSTOMS", "") and os.path.exists(os.environ['ACTIVE_CUSTOMS']):
-            return os.environ['ACTIVE_CUSTOMS']
+            return os.path.dirname(os.environ['ACTIVE_CUSTOMS'])
     return os.path.join(odoo_root(), 'data', 'src', 'customs')
 
 def customs_dir(customs=None):
