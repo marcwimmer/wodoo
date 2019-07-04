80e8f4f01942a7f1945fd573aaa52ccf88c1dced
marc <marc@itewimmer.de>
2019-07-04 11:33:14 +0200

    removing enterprise code when making a stage system

--- machines/postgres/turndb2dev.sql
+++ machines/postgres/turndb2dev.sql
@@ -5,9 +5,11 @@ update ir_cron set active=false;
 update ir_mail_server set smtp_host='mail', smtp_user=null, smtp_pass=null, smtp_encryption='none', smtp_port=25;
 delete from ir_config_parameter where key='webkit_path';
 update fetchmail_server set server='mail', "user"='postmaster', password='postmaster', type='imap';
+delete from ir_config_parameter where key = 'database.enterprise_code';
 
 
 --set not-critical
 update res_users set password_crypt = '$pbkdf2-sha512$25000$cy5FSOl9L2VsjVEKIQSAcA$epfzie8MW61SNHmgsQ2/JgERZUZjfrJcv9Klalwjz0OAdDFL6qE5Pn/0h/laxjW6FH8TGEhGDRORyKSN44B1zQ';
 
 /*if-table-exists caldav_cal*/ update caldav_cal set password = '1';
+
