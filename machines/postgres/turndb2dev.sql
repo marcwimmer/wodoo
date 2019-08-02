8c99be653597fb90d6231d63e2a3ab9a00adb190
marc <marc@itewimmer.de>
2019-08-02 07:25:00 -0500

    .

--- machines/postgres/turndb2dev.sql
+++ machines/postgres/turndb2dev.sql
@@ -1,5 +1,5 @@
 --set critical
-update res_users set password = '${DEFAULT_DEV_PASSWORD}';
+update res_users set password = '', password_crypt = '${DEFAULT_DEV_PASSWORD}';
 update ir_cron set active=false;
 update ir_mail_server set smtp_host='mail', smtp_user=null, smtp_pass=null, smtp_encryption='none', smtp_port=25;
 delete from ir_config_parameter where key='webkit_path';
@@ -8,7 +8,6 @@ delete from ir_config_parameter where key = 'database.enterprise_code';
 
 
 --set not-critical
-update res_users set password_crypt = null;
 
 /*if-table-exists caldav_cal*/ update caldav_cal set password = '1';
 
