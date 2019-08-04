8b5f4a9aea71b566f55136e3b51e594b840c0a88
marc <marc@itewimmer.de>
2019-08-03 19:17:19 -0500

    .

--- machines/odoo/bin/tools.py
+++ machines/odoo/bin/tools.py
@@ -16,7 +16,7 @@ def _replace_params_in_config(ADDONS_PATHS, file):
     content = file.read_text()
     content = content.replace("__ADDONS_PATH__", ADDONS_PATHS)
 
-    server_wide_modules = (os.environ['SERVER_WIDE_MODULES'] or '').split(",")
+    server_wide_modules = (os.environ['SERVER_WIDE_MODULES'] or '')
     # if os.getenv("IS_ODOO_QUEUEJOB", "") == "1" or 'debug' in file.name:
         # server_wide_modules += ',queue_job'
     content = content.replace("__SERVER_WIDE_MODULES__", server_wide_modules)
