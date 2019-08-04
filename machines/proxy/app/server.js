1cb145a66ef88c256f64f3b8e4e4d33f2e7e37cf
marc <marc@itewimmer.de>
2019-08-04 06:40:37 -0500

    .

--- machines/proxy/app/server.js
+++ machines/proxy/app/server.js
@@ -14,6 +14,7 @@ const server_odoo = {
     port: Number(process.env.INTERNAL_ODOO_PORT),
     host: process.env.INTERNAL_ODOO_HOST,
 };
+console.log(server_odoo);
 
 const server_calendar = {
     protocol: 'http',
