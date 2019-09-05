a35693cc74f06efee28a9ff559f222da2f9a61d4
Marc Wimmer <marcwimmer@Marcs-MacBook.local>
2019-09-05 09:05:08 +0200

    Update __after_settings.py

--- images/postgres/__after_settings.py
+++ images/postgres/__after_settings.py
@@ -5,6 +5,6 @@ from pathlib import Path
 import inspect
 
 def after_settings(config):
-    if config['RUN_BTRFS']:
+    if config.get('RUN_BTRFS', ""):
         config['RUN_POSTGRES_IN_BTRFS'] = '1'
         config.write()
