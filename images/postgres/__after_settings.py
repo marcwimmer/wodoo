93b3ac3bd90ea5d01697fd36633536faf4309e88
marc <marc>
2019-09-10 09:17:09 +0000

    .

--- images/postgres/__after_settings.py
+++ images/postgres/__after_settings.py
@@ -5,8 +5,6 @@ from pathlib import Path
 import inspect
 
 def after_settings(config):
-    import pudb
-    pudb.set_trace()
     if config.get('RUN_BTRFS', ""):
         config['RUN_POSTGRES_IN_BTRFS'] = '1'
         config.write()
