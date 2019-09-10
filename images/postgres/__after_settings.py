0d1fde6c82a7d87b176e1af8516d26362adfefb4
marc <marc>
2019-09-10 09:16:20 +0000

    .

--- images/postgres/__after_settings.py
+++ images/postgres/__after_settings.py
@@ -5,10 +5,16 @@ from pathlib import Path
 import inspect
 
 def after_settings(config):
+    import pudb
+    pudb.set_trace()
     if config.get('RUN_BTRFS', ""):
         config['RUN_POSTGRES_IN_BTRFS'] = '1'
         config.write()
 
     customs = config['CUSTOMS']
     dbname = config['DBNAME']
-    config['POSTGRES_VOLUME_NAME'] = "{}_{}_postgres".format(customs, dbname)
+    project_name = os.environ['PROJECT_NAME']
+    if config['DEVMODE'] != '1':
+        config['POSTGRES_VOLUME_NAME'] = "{}_postgres".format(project_name)
+    else:
+        config['POSTGRES_VOLUME_NAME'] = "{}_{}_postgres".format(customs, dbname)
