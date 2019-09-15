4888b0baadec1d58d4df9251713872787369deb6
Marc Wimmer <marcwimmer@Marcs-MacBook.local>
2019-09-15 10:11:12 +0200

    .

--- images/sslproxy/__after_compose.py
+++ images/sslproxy/__after_compose.py
@@ -1,2 +1,7 @@
+import sys
+
 def after_compose(config, yml, globals):
-    pass
+    if config.run_ssl:
+        if not config.sslproxy_subdomains:
+            print("SSLPROXY_SUBDOMAINS missing")
+            sys.exit(1)
