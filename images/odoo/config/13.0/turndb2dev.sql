a6f16a1a73f606ef19cd4a390f17245821c43f30
Marc Wimmer <marcwimmer@Marcs-MacBook.local>
2019-12-23 20:48:53 +0100

    imap access

--- images/odoo/config/13.0/turndb2dev.sql
+++ images/odoo/config/13.0/turndb2dev.sql
@@ -1,9 +1,9 @@
 --set critical
 update res_users set password = '${DEFAULT_DEV_PASSWORD}';
 update ir_cron set active=false;
-update ir_mail_server set smtp_host='${TEST_MAIL_HOST}', smtp_user=null, smtp_pass=null, smtp_encryption='none', smtp_port=${TEST_MAIL_PORT};
 delete from ir_config_parameter where key='webkit_path';
-update fetchmail_server set server='mail', "user"='postmaster', password='postmaster', server_type='imap';
+update ir_mail_server set smtp_host='${TEST_MAIL_HOST}', smtp_user=null, smtp_pass=null, smtp_encryption='none', smtp_port=${TEST_MAIL_SMTP_PORT};
+update fetchmail_server set server='${TEST_MAIL_HOST}', port='${TEST_MAIL_IMAP_PORT}', "user"='postmaster', password='postmaster', type='imap';
 delete from ir_config_parameter where key = 'database.enterprise_code';
 
 
