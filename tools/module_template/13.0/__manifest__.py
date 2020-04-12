95dd906207416132cdd790245066ef321a212876
Marc Wimmer <marc@itewimmer.de>
2020-04-12 23:31:36 +0200

    updated manifest of template project

--- tools/module_template/13.0/__manifest__.py
+++ tools/module_template/13.0/__manifest__.py
@@ -7,6 +7,6 @@
     'data': [],
     "external_dependencies": {"python": [], "bin": []},
     #"uninstall_hook": "uninstall_hook",
-    #'pre_init_hook': 'post_init_hook',
+    #'pre_init_hook': 'pre_init_hook',
     #'post_init_hook': 'post_init_hook',
 }
