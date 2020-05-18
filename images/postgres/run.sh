364d3f4303fb401e30c26507211825ff00282d96
Marc Wimmer <marc@itewimmer.de>
2020-05-18 08:31:14 +0200

    changing entrypoint postgres scripts to executable
    did not work out of the box at a new production server

--- images/postgres/run.sh
+++ images/postgres/run.sh
@@ -5,4 +5,5 @@ CONFIG="$(sed 's/^/-c/' /config)"
 
 cp /tmp/docker-entrypoint-initdb.d/* /docker-entrypoint-initdb.d
 chown -R postgres:postgres /docker-entrypoint-initdb.d 
+chmod a+x /docker-entrypoint-initdb.d/*.sh
 exec gosu postgres /docker-entrypoint.sh postgres $CONFIG
