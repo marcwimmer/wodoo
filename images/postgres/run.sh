e4d74f3ca1313d26b8c3db54c406b9a4ac9c2bab
Marc Wimmer <marc@itewimmer.de>
2020-06-09 21:02:20 +0200

    required fix in postgres entrypoint .... dunno why

--- images/postgres/run.sh
+++ images/postgres/run.sh
@@ -3,7 +3,7 @@ set -x
 set -e
 CONFIG="$(sed 's/^/-c/' /config)"
 
-cp /tmp/docker-entrypoint-initdb.d/* /docker-entrypoint-initdb.d
-chown -R postgres:postgres /docker-entrypoint-initdb.d 
-chmod a+x /docker-entrypoint-initdb.d/*.sh
+cp /tmp/docker-entrypoint-initdb.d/* /docker-entrypoint-initdb.d || true
+chown -R postgres:postgres /docker-entrypoint-initdb.d  || true
+chmod a+x /docker-entrypoint-initdb.d/*.sh || true
 exec gosu postgres /docker-entrypoint.sh postgres $CONFIG
