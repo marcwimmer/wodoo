6ea351643958410b97af773c8ece01dccf3e2e0b
hetzi1 <hetzi1>
2020-09-04 13:21:04 +0200

    WIP

--- images/postgres/run.sh
+++ images/postgres/run.sh
@@ -3,7 +3,6 @@ set -x
 set -e
 CONFIG="$(sed 's/^/-c/' /config)"
 
-cp /tmp/docker-entrypoint-initdb.d/* /docker-entrypoint-initdb.d || true
-chown -R postgres:postgres /docker-entrypoint-initdb.d  || true
-chmod a+x /docker-entrypoint-initdb.d/*.sh || true
-exec gosu postgres /docker-entrypoint.sh postgres $CONFIG
+
+#exec gosu postgres /usr/local/bin/docker-entrypoint.sh postgres $CONFIG
+echo "BYE"
