397ad3f2ee818560829afbfb08e79c55297d7456
Marc Wimmer <marc@itewimmer.de>
2020-10-01 12:13:03 +0200

    Upgraded letsencrypt to latest server (linuxserver/swag)

--- images/sslproxy/__commands.py
+++ images/sslproxy/__commands.py
@@ -24,45 +24,10 @@ def _safe_delete(f):
     if f.exists():
         f.unlink()
 
-@sslproxy.command()
-@click.option('--test', is_flag=True, help="Set if you're testing your setup to avoid hitting request limits")
-@pass_config
-@click.pass_context
-def init(ctx, config, test):
-    click.secho("Caution: Port 443 and 80 must be temporarily accessible.", fg='yellow', bold=True)
-    composefile = files['docker_compose']
-    y = yaml.safe_load(composefile.read_text())
-    y['services']['letsencrypt']['ports'] = [
-        "0.0.0.0:80:80",
-        "0.0.0.0:443:443",
-    ]
-    composefile.write_text(yaml.dump(y))
-    __dc([
-        'kill',
-        'sslproxy',
-        'letsencrypt'
-    ])
-    __dc([
-        'rm',
-        '-f',
-        'letsencrypt'
-    ])
 
-    __dc([
-        'up',
-        'letsencrypt'
-    ])
-    __dc([
-        'rm',
-        '-f',
-        'letsencrypt'
-    ])
-    y['services']['sslproxy']['ports'] = []
-    composefile.write_text(yaml.dumps(y))
-
-@sslproxy.command()
+@sslproxy.command(help="Removes existing SSL certificates; restart to renew them")
 @pass_config
 @click.pass_context
 def clean(ctx, config):
-    path = Path(config.HOST_RUN_DIR) / 'ssl'
+    path = Path(config.dirs['run']) / 'ssl'
     __empty_dir(path, user_out=True)
