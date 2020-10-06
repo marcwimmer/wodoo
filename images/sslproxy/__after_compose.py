215616c1d841d370c10aa8e75c26b658dcc1832d
hetzi1 <hetzi1>
2020-10-06 23:13:51 +0200

    removed trash

--- images/sslproxy/__after_compose.py
+++ images/sslproxy/__after_compose.py
@@ -23,20 +23,3 @@ def after_compose(config, settings, yml, globals):
     click.secho(f"IMPORTANT:", fg='green', bold=True)
     click.secho(f"1. Please start the sslproxy once.", fg='green')
     click.secho(f"2. call odoo sslproxy reset", fg='green')
-
-    # nginx_conf =
-    # src = (dir / 'nginx.conf.template').read_text()
-    # subdomains = settings.get('SSLPROXY_SUBDOMAINS', "")
-    # if subdomains:
-        # subdomains = subdomains.split(",")
-        # if len(subdomains) > 1:
-            # raise Exception("only one subdomain supported")
-
-        # domain = subdomains[0] + "." + settings['SSLPROXY_DOMAIN']
-    # else:
-        # domain = settings['SSLPROXY_DOMAIN']
-
-    # src = src.replace("__DOMAIN__", domain)
-
-    # nginx_conf.parent.mkdir(parents=True, exist_ok=True)
-    # nginx_conf.write_text(src)
