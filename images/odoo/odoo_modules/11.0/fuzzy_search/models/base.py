89a049be4b9169b80a37b40b9f8665a2a5a654a3
hetzi1 <hetzi1>
2020-12-10 21:39:54 +0100

    fuzzzy

--- images/odoo/odoo_modules/11.0/fuzzy_search/models/base.py
+++ images/odoo/odoo_modules/11.0/fuzzy_search/models/base.py
@@ -5,24 +5,18 @@ from odoo.exceptions import UserError, RedirectWarning, ValidationError
 
     # @api.model
     # def create(self, vals):
-        # from pudb import set_trace
-        # set_trace()
         # result = super().create(vals)
         # if 'fuzzy.mixin' in getattr(self, '_inherits', []):
             # result._update_fuzzy_words()
         # return result
 
     # def write(self, vals):
-        # from pudb import set_trace
-        # set_trace()
         # result = super().write(vals)
         # if any(x in vals for x in self._get_fuzzy_fields()):
             # self._update_fuzzy_words()
         # return result
 
     # def unlink(self):
-        # from pudb import set_trace
-        # set_trace()
         # self.env['fuzzy.words'].sudo().search([
             # ('res_model', '=', self._name),
             # ('res_id', 'in', self.ids)
