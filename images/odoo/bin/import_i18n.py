38d61c3e9bba0512df865a3f4d3f2a3a45972154
hetzi1 <hetzi1>
2020-12-11 21:21:25 +0100

    fixed translation import

--- images/odoo/bin/import_i18n.py
+++ images/odoo/bin/import_i18n.py
@@ -5,6 +5,7 @@ import os
 import sys
 import subprocess
 from odoo_tools.module_tools import Module
+from odoo_tools.odoo_config import get_odoo_addons_paths
 from pathlib import Path
 from tools import exec_odoo
 if len(sys.argv) == 1:
@@ -19,12 +20,20 @@ if len(sys.argv) == 2:
 LANG = sys.argv[1]
 FILEPATH = sys.argv[2]
 
+addon_paths = get_odoo_addons_paths
+for path in addon_paths():
+    if (path / FILEPATH).exists():
+        filepath = path / FILEPATH
+        break
+else:
+    print(f"File not found: {FILEPATH}")
+    sys.exit(-1)
 print(f"Importing lang file {FILEPATH}")
 
 exec_odoo(
     'config_i18n',
     '--stop-after-init',
     '-l', LANG,
-    '--i18n-import={}'.format(FILEPATH),
+    '--i18n-import={}'.format(filepath),
     '--i18n-overwrite',
 )
