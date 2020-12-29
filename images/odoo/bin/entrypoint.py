0e00b67ff634cb119b5aef6e7d927d9886b73143
hetzi1 <hetzi1>
2020-12-29 21:50:29 +0100

    HOTFIX updating modules only in devmode

--- images/odoo/bin/entrypoint.py
+++ images/odoo/bin/entrypoint.py
@@ -5,16 +5,13 @@ import shutil
 from pathlib import Path
 
 owner = os.environ['OWNER_UID']
-ODOO_SERVER_TOOLS_MODULES = os.environ['ODOO_SERVER_TOOLS_MODULES']
 
 os.system(f"usermod -u {owner} odoo")
 
 print(f"Setting ownership of /opt/files to {owner}")
 os.system(f"chown '{owner}:{owner}' /opt/files")
-# os.system(f"rsync /tmp/odoo_server_tools.template/ {ODOO_SERVER_TOOLS_MODULES} -ar")
 os.system("python3 /odoolib/put_server_modules_into_odoo_src_dir.py")
 
-os.system(f"chown '{owner}:{owner}' -R {ODOO_SERVER_TOOLS_MODULES}")
 # important is especially the .config folder, so that libreoffice works
 print(f"Setting ownership of /home/odoo to {owner}")
 os.system(f"chown -R '{owner}:{owner}' /home/odoo")
--- images/odoo/bin/put_server_modules_into_odoo_src_dir.py
+++ images/odoo/bin/put_server_modules_into_odoo_src_dir.py
@@ -5,13 +5,13 @@ import shutil
 from pathlib import Path
 from odoo_tools.odoo_config import customs_dir
 
-owner = os.environ['OWNER_UID']
-ODOO_SERVER_TOOLS_MODULES = os.environ['ODOO_SERVER_TOOLS_MODULES']
+if os.getenv("DEVMODE", "") == "1":
+    owner = os.environ['OWNER_UID']
 
 # also update the source in addons_tools, so that developers without using this framework
 # have the modules in a sub path
-for tool_module_dir in Path("/tmp/odoo_server_tools.template").glob("*"):
-    if tool_module_dir.is_dir():
-        dest_path = f'{customs_dir()}/addons_tools/{tool_module_dir.name}'
-        os.system(f"rsync {tool_module_dir}/ {dest_path}/  -ar --delete-after")
-        os.system(f"chown -R {owner}:{owner} '{dest_path}' ")
+    for tool_module_dir in Path("/tmp/odoo_server_tools.template").glob("*"):
+        if tool_module_dir.is_dir():
+            dest_path = f'{customs_dir()}/addons_tools/{tool_module_dir.name}'
+            os.system(f"rsync {tool_module_dir}/ {dest_path}/  -ar --delete-after")
+            os.system(f"chown -R {owner}:{owner} '{dest_path}' ")
