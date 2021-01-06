d60aa7a0730d7b77650207603d731b94aee5af40
hetzi1 <hetzi1>
2021-01-06 17:24:46 +0100

    removed fssync

--- images/fssync/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-usermod -u $OWNER_UID unison
-echo "Starting unison server..."
-chmod a+rwx /opt/target
-chown $OWNER_UID -R /opt/target
-su - unison -c "unison -socket 14122 -host 0.0.0.0"
