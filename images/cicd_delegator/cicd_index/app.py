2fcfeb7c3d0e8fbc8282a9e248705deec24ef3e0
hetzi1 <hetzi1>
2021-01-29 21:25:36 +0100

    RC2701
    images/cicd_delegator/cicd_index/app.py

--- images/cicd_delegator/cicd_index/app.py
+++ images/cicd_delegator/cicd_index/app.py
@@ -17,6 +17,10 @@ def augment_reg(reg):
         if last_access_file.exists():
             site['last_access'] = arrow.get(last_access_file.read_text()).to(os.environ['DISPLAY_TIMEZONE'])
 
+@app.route('/register/site')
+def register_site(**kwargs):
+    raise Exception(kwargs)
+
 
 @app.route('/')
 def index():
