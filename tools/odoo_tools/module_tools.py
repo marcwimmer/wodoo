44470c5500db2e353967bc0da7cc47d882c58fc1
hetzi1 <hetzi1>
2021-01-29 14:31:56 +0100

    RC2701
    tools/odoo_tools/module_tools.py

--- tools/odoo_tools/module_tools.py
+++ tools/odoo_tools/module_tools.py
@@ -502,7 +502,7 @@ class Modules(object):
         filepaths = subprocess.check_output([
             'git',
             'diff',
-            f"{shastart}..HEAD",
+            f"{sha_start}..HEAD",
             "--name-only",
         ]).decode('utf-8').split("\n")
         modules = []
