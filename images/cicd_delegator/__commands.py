947c4233b8f4e5760f27dc7f59f466b8e505098b
hetzi1 <hetzi1>
2021-01-29 14:41:21 +0100

    RC2701
    images/cicd_delegator/__commands.py

--- images/cicd_delegator/__commands.py
+++ images/cicd_delegator/__commands.py
@@ -129,8 +129,13 @@ def register(ctx, config, desc, author, local, title, initiator, git_branch, git
         'sha': git_sha,
     }
     site['enabled'] = False
+    site['modules_updated'] = []
     # get the previous instance by branch
     if current_instance:
+        current_sha = current_instance.get('git', {}).get('sha')
+        if current_sha:
+            site['modules_updates'] = Modules.get_changed_modules(current_sha):
+
 
 
     set_registry(config, reg)
