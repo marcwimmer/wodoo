f7ee8c353d713c8c106abe524101a7e24636aad8
hetzi1 <hetzi1>
2021-01-30 22:01:24 +0100

    RC2701
    images/cicd_delegator/cicd_index/app.py

--- images/cicd_delegator/cicd_index/app.py
+++ images/cicd_delegator/cicd_index/app.py
@@ -136,7 +136,7 @@ def index():
             site['updated'] = arrow.get(site['updated']).to(os.environ['DISPLAY_TIMEZONE'])
     sites = sorted(sites, key=lambda x: x.get('updated', x.get('last_access', arrow.get('1980-04-04'))), reverse=True)
 
-    sites = list(groupby(sites, itemgetter('git_branch')))
+    sites = list(groupby(sites, key=itemgetter('git_branch')))
 
     return render_template(
         'index.html',
