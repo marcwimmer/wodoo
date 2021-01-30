89171abc933954416c67f2b40cccaa19b46a4884
hetzi1 <hetzi1>
2021-01-30 13:58:08 +0100

    RC2701
    images/cicd_delegator/__commands.py

--- images/cicd_delegator/__commands.py
+++ images/cicd_delegator/__commands.py
@@ -122,7 +122,7 @@ def register(ctx, config, desc, author, local, title, initiator, git_branch, git
         if current_sha:
             site['diff_modules'] = Modules.get_changed_modules(current_sha)
 
-    requests.post(url + '/register', data=site).raise_for_status()
+    requests.post(url + '/register', json=site).raise_for_status()
     Commands.invoke(
         ctx,
         'up',
