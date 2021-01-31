a9aca93d4eb27ddc196fa23d73a5a82e730fbd12
hetzi1 <hetzi1>
2021-01-31 23:25:52 +0100

    RC2701
    images/cicd_delegator/cicd_index/templates/static/js/app.js

--- images/cicd_delegator/cicd_index/templates/static/js/app.js
+++ images/cicd_delegator/cicd_index/templates/static/js/app.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 
     $(".docker-control").click(function() {
         var $el = $(this);
+        var action = $el.data('action')
+        var name = $el.data('name');
     });
     alert("HI");
 });
