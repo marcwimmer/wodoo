e9c439223b5844313c630ba5fd774461178dd078
hetzi1 <hetzi1>
2021-01-31 21:19:48 +0100

    RC2701
    tools/odoo_tools/module_tools.py

--- tools/odoo_tools/module_tools.py
+++ tools/odoo_tools/module_tools.py
@@ -721,6 +721,8 @@ class Module(object):
         self.version = float(current_version())
         path = Path(path)
         p = path if path.is_dir() else path.parent
+        import pudb
+        pudb.set_trace()
 
 
         # for p in [p] + list(p.parents):
