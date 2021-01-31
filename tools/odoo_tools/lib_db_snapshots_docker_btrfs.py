bed01bf96a04c075bd927a62cfa23bd8e548b5c4
hetzi1 <hetzi1>
2021-01-31 10:05:32 +0100

    RC2701
    tools/odoo_tools/lib_db_snapshots_docker_btrfs.py

--- tools/odoo_tools/lib_db_snapshots_docker_btrfs.py
+++ tools/odoo_tools/lib_db_snapshots_docker_btrfs.py
@@ -111,6 +111,7 @@ def restore(config, name):
     if not name:
         return
     __dc(['stop', '-t 1'] + ['postgres'])
+    breakpoint()
     volume_path = Path("/var/lib/docker/volumes") / __get_postgres_volume_name(config)
     if volume_path.exists():
         subprocess.check_call(
