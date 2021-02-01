1601cba1375a0b41aadef895d7d942a54a0e250c
hetzi1 <hetzi1>
2021-02-01 12:14:42 +0100

    RC2701
    images/cicd_delegator/cicd_index/app.py

--- images/cicd_delegator/cicd_index/app.py
+++ images/cicd_delegator/cicd_index/app.py
@@ -145,7 +145,6 @@ def register_site():
         })
         sites = sorted(sites, key=lambda x: x['index'])
         site['enabled'] = False
-        site['name'] = f"{site['git_branch']}_{site['key']}_{index}"
         db.sites.insert_one(site)
         return jsonify({'result': 'ok', 'name': site['name']})
 
