aea3a6ed6e2f3e856b00bc52dc78fb4335024d63
hetzi1 <hetzi1>
2021-02-01 09:51:50 +0100

    RC2701
    images/cicd_delegator/cicd_index/app.py

--- images/cicd_delegator/cicd_index/app.py
+++ images/cicd_delegator/cicd_index/app.py
@@ -20,6 +20,10 @@ import threading
 import logging
 import jenkins
 
+jenkins = jenkins.Jenkins('http://192.168.101.122:8080', username='admin', password='1')
+print(f"Jenkins {jenkins.get_whoami()} and version {jenkins.get_version()}")
+
+
 
 from pymongo import MongoClient
 mongoclient = MongoClient(
