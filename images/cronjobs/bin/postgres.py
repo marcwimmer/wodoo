36a0eeebd0bab8b636212c896b2db7ae0328ee08
hetzi1 <hetzi1>
2021-02-02 15:00:37 +0100

    RC2701
    images/cronjobs/bin/postgres.py

--- images/cronjobs/bin/postgres.py
+++ images/cronjobs/bin/postgres.py
@@ -102,8 +102,8 @@ def _restore(dbname, host, port, user, password, filepath):
     if not dbname:
         raise Exception("DBName missing")
 
-    os.system(f"echo 'drop database if exists {dbname};' | psql {" ".join(args)} template1"
-    os.system(f"echo 'create database {dbname};' | psql {" ".join(args)} template1"
+    os.system(f"echo 'drop database if exists {dbname};' | psql {" ".join(args)} template1")
+    os.system(f"echo 'create database {dbname};' | psql {" ".join(args)} template1")
 
     method = PGRESTORE
     needs_unzip = True
