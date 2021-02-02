6d109a1b20b139291484c843830510ba2e981b80
hetzi1 <hetzi1>
2021-02-02 14:55:46 +0100

    RC2701
    tools/odoo_tools/lib_backup.py

--- tools/odoo_tools/lib_backup.py
+++ tools/odoo_tools/lib_backup.py
@@ -85,7 +85,7 @@ def backup_calendar(config):
 @click.pass_context
 @click.argument('filename', required=False, default="")
 @click.option('--dbname', required=False)
-@click.option('--dumptype', type=click.Choice(["custom", "plain"]), case_sensitive=False, default='custom')
+@click.option('--dumptype', type=click.Choice(["custom", "plain"]), default='custom')
 def backup_db(ctx, config, filename, dbname, dumptype):
     filename = filename or f'{config.project_name}.{config.dbname}.odoo' + '.dump.gz'
     cmd = [
