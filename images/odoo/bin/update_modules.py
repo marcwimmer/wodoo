3bfc3a9b0a1939e11d50cf773a4ec288c70c04d6
hetzi1 <hetzi1>
2021-02-04 22:44:07 +0100

    RC2701
    images/odoo/bin/update_modules.py

--- images/odoo/bin/update_modules.py
+++ images/odoo/bin/update_modules.py
@@ -93,7 +93,7 @@ def update(config, mode, modules):
                         params = [
                             '-l',
                             lang,
-                            f'--i18n-import={module.path}/i18n/{lang}.po',
+                            f'--i18n-import={module.path}/i18n/{lang_file.name}.po',
                             '--i18n-overwrite',
                             '--stop-after-init',
                         ]
