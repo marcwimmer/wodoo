2534d43ec0fcf626abba8ce6c8c1dc74b7ac7cef
hetzi1 <hetzi1>
2021-03-19 10:40:30 +0100

    HOTFIX

--- images/postgres/run.sh
+++ images/postgres/run.sh
@@ -6,7 +6,7 @@ print("Version 1.0")
 import os
 with open('/config') as file:
     conf = file.read().split("\n")
-conf += os.environ['POSTGRES_CONFIG'].split(",")
+conf += os.getenv('POSTGRES_CONFIG').split(",")
 conf = list(filter(lambda x: bool((x or '').strip()), conf))
 
 print("Applying configuration:\n" + '\n'.join(conf))
