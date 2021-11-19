43da5fa6d8e62f11127f9f4b7609f5f0f1dd339e
rs-feature03 <rs-feature03>
2021-11-19 15:22:04 +0000

    good uninstaller

--- images/odoo/odoo_modules/14.0/server_tools_uninstaller/__manifest__.py
+++
@@ -1,9 +0,0 @@
-
-{
-    'name': 'server_tools_uninstaller',
-    'version': '14.0.1.0',
-    'author': 'Marc Wimmer (marc@itewimmer.de)',
-    'depends': ['base'],
-    'data': ['data.xml'],
-    "external_dependencies": {"python": [], "bin": []},
-}
