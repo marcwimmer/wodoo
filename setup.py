af1fd55ddfb221aac3571071d4d01b9dbdbc0abc
Your Name <you@example.com>
2022-01-03 10:25:29 +0000

    artefacts

--- setup.py
+++ setup.py
@@ -8,10 +8,12 @@ import shutil
 import io
 import os
 import sys
+import json
 import requests
 from glob import glob
 from shutil import rmtree
 from pathlib import Path
+from setuptools.config import read_configuration
 
 from setuptools import find_packages, setup, Command
 from setuptools.command.install import install
@@ -21,18 +23,11 @@ import inspect
 import os
 from pathlib import Path
 current_dir = Path(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))))
+setup_cfg = read_configuration("setup.cfg")
+metadata = setup_cfg['metadata']
 
 
 # Package meta-data.
-NAME = 'wodoo'
-SHELL_CALL = "odoo"
-DESCRIPTION = 'Odoo Framework'
-URL = 'https://git.itewimmer.de/odoo/framework'
-EMAIL = 'marc@itewimmer.de'
-AUTHOR = 'Marc-Christian Wimmer'
-REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.0'
-
 # What packages are required for this module to be executed?
 REQUIRED = list(filter(bool, (current_dir / 'wodoo' / 'requirements.txt').read_text().split("\n")))
 
@@ -54,16 +49,16 @@ try:
     with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
         long_description = '\n' + f.read()
 except FileNotFoundError:
-    long_description = DESCRIPTION
+    long_description = metadata['DESCRIPTION']
 
 # Load the package's __version__.py module as a dictionary.
 about = {}
-if not VERSION:
-    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+if not metadata['version']:
+    project_slug = metadata['name'].lower().replace("-", "_").replace(" ", "_")
     with open(os.path.join(here, project_slug, '__version__.py')) as f:
         exec(f.read(), about)
 else:
-    about['__version__'] = VERSION
+    about['__version__'] = metadata['version']
 
 
 class UploadCommand(Command):
@@ -115,11 +110,19 @@ class InstallCommand(install):
         self.download_artefacts()
 
     def download_artefacts(self):
-        path = Path(self.install_lib) / NAME
-        path_robot_artefacts = path / 'images' / 'robot' / 'artefacts'
-        path_robot_artefacts.mkdir(exist_ok=True)
-        self.download_file_and_move('https://github.com/marcwimmer/chromedrivers/raw/2021-12/chromedriver_amd64.zip', path_robot_artefacts)
-        self.download_file_and_move('https://github.com/marcwimmer/chromedrivers/raw/2021-12/googlechrome_amd64.deb', path_robot_artefacts)
+        path = Path("artefacts")
+        if not path.exists():
+            return
+        artefacts = json.loads(path.read_text())
+        base_path = Path(self.install_lib) / metadata['name']
+        for path, files in artefacts.items():
+            path = base_path / path
+            path.mkdir(exist_ok=True, parents=True)
+            for file in files:
+                self.download_file_and_move(
+                    file,
+                    path
+                )
 
     def download_file_and_move(self, url, dest_parent_path):
         file = self.download_file(url)
@@ -136,35 +139,34 @@ class InstallCommand(install):
 
     def setup_click_autocompletion(self):
 
-        def setup_for_shell_generic(shell):
+        def setup_for_shell_generic(shell, shell_call):
             path = Path(f"/etc/{shell}_completion.d")
             completion = (Path("completions") / f"odoo.{shell}").read_bytes()
             if path.exists():
                 if os.access(path, os.W_OK):
-                    (path / SHELL_CALL).write_bytes(completion)
+                    (path / shell_call).write_bytes(completion)
                     return
 
-            if not (path / NAME).exists():
+            if not (path / shell_call).exists():
                 rc = Path(os.path.expanduser("~")) / f'.{shell}rc'
                 if not rc.exists():
                     return
-                complete_file = rc.parent / f'.{SHELL_CALL}-completion.sh'
+                complete_file = rc.parent / f'.{shell_call}-completion.sh'
                 complete_file.write_bytes(completion)
                 if complete_file.name not in rc.read_text():
                     content = rc.read_text()
                     content += '\nsource ~/' + complete_file.name
                     rc.write_text(content)
 
-        setup_for_shell_generic('zsh')
-        setup_for_shell_generic('bash')
-        setup_for_shell_generic('fish')
-
+        for console_script in setup_cfg['options']['entry_points']['console_scripts']:
+            shell_call = console_script.split("=")[0].strip()
+            for console in ['zsh', 'bash', 'fish']:
+                setup_for_shell_generic(console, shell_call)
 
 
 def get_data_files():
     data_files = []
-    PREFIX = Path(sys.prefix) / 'local' / NAME
-    for i, file in enumerate((current_dir / 'wodoo').rglob("*")):
+    for i, file in enumerate((current_dir / metadata['name']).rglob("*")):
         if not file.is_file():
             continue
         if any(file.name.endswith(x) for x in ['.pyc', '.py']):
@@ -181,36 +183,15 @@ def get_data_files():
 
 # Where the magic happens:
 setup(
-    name=NAME,
     version=about['__version__'],
-    description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author=AUTHOR,
-    author_email=EMAIL,
-    python_requires=REQUIRES_PYTHON,
-    url=URL,
-    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
     # If your package is a single module, use this instead of 'packages':
     #py_modules=['prlsnapshotter'],
     data_files=get_data_files(),
-    entry_points={
-        'console_scripts': [SHELL_CALL + '=wodoo:cli'],
-    },
     install_requires=REQUIRED,
     extras_require=EXTRAS,
     include_package_data=True,
-    license='MIT',
-    classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
-    ],
     # $ setup.py publish support.
     cmdclass={
         'upload': UploadCommand,
