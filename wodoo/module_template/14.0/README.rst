8a7e0f5257da30b84b76a11500f3c46d23aa7ce5
marc <marc@itewimmer.de>
2022-01-14 21:50:00 +0100

    module templates

---
+++ wodoo/module_template/14.0/README.rst
@@ -0,0 +1,18 @@
+How to use
+~~~~~~~~~~~~
+
+.. code-block:: sql
+
+   update table x
+
+How to use::
+
+   self.env['...']
+
+.. image:: /___module_name___/static/sample.png
+
+Authors
+~~~~~~~~~~~~~~~
+
+* Marc Wimmer <marc@itewimmer.de>
+
---
+++ wodoo/module_template/14.0/__manifest__.py
@@ -0,0 +1,12 @@
+
+{
+    'name': '___module_name___',
+    'version': '14.0.1.0',
+    'author': 'Marc Wimmer (marc@itewimmer.de)',
+    'depends': ['base'],
+    'data': [],
+    "external_dependencies": {"python": [], "bin": []},
+    #"uninstall_hook": "uninstall_hook",
+    #'pre_init_hook': 'pre_init_hook',
+    #'post_init_hook': 'post_init_hook',
+}
