9b15e304ee3c08b6de2dbb5fdbac474ef95b9f9a
odoo <odoo>
2022-06-08 21:43:28 +0200

    Revert "blacked"
    This reverts commit 88228b0ea4c52aa6e480fffb211f1bd2d64a1457.

--- wodoo/lib_control_native.py
+++ wodoo/lib_control_native.py
@@ -8,12 +8,10 @@ from . import cli, pass_config, Commands
 from .lib_clickhelpers import AliasedGroup
 import subprocess
 
-
 @cli.group(cls=AliasedGroup)
 @pass_config
 def control(config):
     pass
-
 
 @control.command()
 @pass_config
@@ -23,83 +21,68 @@ def dev(ctx, config):
     starts developing in the odoo container
     """
     from .myconfigparser import MyConfigParser
-
-    myconfig = MyConfigParser(config.files["settings"])
-    proxy_port = myconfig["PROXY_PORT"]
-    roundcube_port = myconfig["ROUNDCUBE_PORT"]
-    ip = "127.0.0.1"
-    click.secho(
-        "Proxy Port: http://{}:{}".format(ip, proxy_port), fg="green", bold=True
-    )
-    click.secho(
-        "Mailclient : http://{}:{}".format(ip, roundcube_port), fg="green", bold=True
-    )
-    _exec_in_virtualenv(config, ["python", _path_odoolib() / "debug.py"])
+    myconfig = MyConfigParser(config.files['settings'])
+    proxy_port = myconfig['PROXY_PORT']
+    roundcube_port = myconfig['ROUNDCUBE_PORT']
+    ip = '127.0.0.1'
+    click.secho("Proxy Port: http://{}:{}".format(ip, proxy_port), fg='green', bold=True)
+    click.secho("Mailclient : http://{}:{}".format(ip, roundcube_port), fg='green', bold=True)
+    _exec_in_virtualenv(config, ['python', _path_odoolib() / 'debug.py'])
 
 
 def _update_command(config, params):
-    _exec_in_virtualenv(
-        config, ["python", _path_odoolib() / "update_modules.py"] + params
-    )
+    _exec_in_virtualenv(config, ['python', _path_odoolib() / 'update_modules.py'] + params)
 
 
 def up(ctx, config, machines=[], daemon=False, remove_orphans=True):
-    _exec_in_virtualenv(config, ["python", _path_odoolib() / "run.py"])
+    _exec_in_virtualenv(config, ['python', _path_odoolib() / 'run.py'])
 
 
 @control.command()
 def shell(config):
-    subprocess.call(["python", ""])
-    _exec_in_virtualenv(config, ["python", _path_odoolib() / "shell.py"])
-
+    subprocess.call([
+        'python',
+        ''
+    ])
+    _exec_in_virtualenv(config, ['python', _path_odoolib() / 'shell.py'])
 
 def _path_odoolib():
-    return Path(os.environ["ODOO_HOME"]) / "images" / "odoo" / "bin"
-
+    return Path(os.environ['ODOO_HOME']) / 'images' / 'odoo' / 'bin'
 
 def _exec_in_virtualenv(config, cmd):
-    filename = Path(tempfile.mktemp(suffix=".sh"))
+    filename = Path(tempfile.mktemp(suffix='.sh'))
     from .myconfigparser import MyConfigParser
-
-    myconfig = MyConfigParser(config.files["settings"])
+    myconfig = MyConfigParser(config.files['settings'])
 
     def _quoted(x):
         return "'{}'".format(x)
-
     content = []
     for key in myconfig.keys():
         content.append("export {}='{}'".format(key, myconfig.get(key)))
     files = config.files
     dirs = config.dirs
 
-    raise Exception("adapt after pip compatible")
+    raise Exception('adapt after pip compatible')
 
-    filename.write_text(
-        "\n".join(
-            content
-            + list(
-                (
-                    f"set -ex",
-                    f'export DEBUGGER_WATCH="{files["run/odoo_debug.txt"]}"',
-                    f'export ODOO_CONFIG_TEMPLATE_DIR="{dirs["images"] / "odoo" / "config" / str(current_version()) / "config"}"',
-                    f'export ODOO_CONFIG_DIR="{dirs["run_native_config_dir"]}"',
-                    f'export ODOOLIB="{dirs["images"] / "odoo" / "bin"}"',
-                    f'export ODOO_USER="$(whoami)"',
-                    f'export ODOO_DATA_DIR="{dirs["odoo_data_dir"]}"',
-                    f'export SERVER_DIR="{customs_dir()}/odoo"',
-                    f'export PYTHONPATH="{dirs["."]}"',
-                    f'export RUN_DIR="{dirs["run"]}"',
-                    f"export NO_SOFFICE=1",
-                    f'export OUT_DIR="{dirs["run_native_out_dir"]}"',
-                    f"export INTERNAL_ODOO_PORT=8069",
-                    f"source '{dirs['venv']}/bin/activate'",
-                    f"which python",
-                    f"python --version",
-                    f"{' '.join(map(_quoted, cmd))}",
-                )
-            )
-        )
-    )
+    filename.write_text('\n'.join(content + list((
+        f'set -ex',
+        f'export DEBUGGER_WATCH="{files["run/odoo_debug.txt"]}"',
+        f'export ODOO_CONFIG_TEMPLATE_DIR="{dirs["images"] / "odoo" / "config" / str(current_version()) / "config"}"',
+        f'export ODOO_CONFIG_DIR="{dirs["run_native_config_dir"]}"',
+        f'export ODOOLIB="{dirs["images"] / "odoo" / "bin"}"',
+        f'export ODOO_USER="$(whoami)"',
+        f'export ODOO_DATA_DIR="{dirs["odoo_data_dir"]}"',
+        f'export SERVER_DIR="{customs_dir()}/odoo"',
+        f'export PYTHONPATH="{dirs["."]}"',
+        f'export RUN_DIR="{dirs["run"]}"',
+        f'export NO_SOFFICE=1',
+        f'export OUT_DIR="{dirs["run_native_out_dir"]}"',
+        f'export INTERNAL_ODOO_PORT=8069',
+        f"source '{dirs['venv']}/bin/activate'",
+        f"which python",
+        f"python --version",
+        f"{' '.join(map(_quoted, cmd))}",
+    ))))
 
     subprocess.call(["/bin/bash", filename])
     print("--------------------------------------")
--- wodoo/robo_helpers.py
+++ wodoo/robo_helpers.py
@@ -6,7 +6,6 @@ import tempfile
 import os
 import base64
 
-
 def collect_all(root_dir, subdir_name, glob, dest_folder):
     """collects files in all directories by glob pattern being in a directory with subdir name and copies
     the files to dest_folder
@@ -25,24 +24,20 @@ def collect_all(root_dir, subdir_name, glob, dest_folder):
         for file in files:
             dest_path = dest_folder / subdir_name / file.name
             if dest_path.exists():
-                click.secho(
-                    f"Warning: Path {dest_path} already exists and is overwritten by {file}.",
-                    fg="yellow",
-                )
-                # raise Exception(f"Destination path '{dest_path}' already exists.")
+                click.secho(f"Warning: Path {dest_path} already exists and is overwritten by {file}.", fg='yellow')
+                #raise Exception(f"Destination path '{dest_path}' already exists.")
             dest_path.parent.mkdir(exist_ok=True)
             shutil.copy(file, dest_path)
-            yield from _get_required_odoo_modules_from_robot_file(file.read_text())
-
+            yield from _get_required_odoo_modules_from_robot_file(
+                file.read_text())
 
 def _get_required_odoo_modules_from_robot_file(filecontent):
     lines = filecontent.split("\n")
     for line in lines:
-        if "odoo-require:" in line:
+        if 'odoo-require:' in line:
             line = line.split(":", 1)[1].strip().split(",")
             line = [x.strip() for x in line]
             yield from line
-
 
 def _make_archive(verbose, test_files, root_dir):
     """Makes archive of robot test containing all aggregated keywords.
@@ -54,7 +49,7 @@ def _make_archive(verbose, test_files, root_dir):
         bytes: the archive in bytes format
     """
     working_space = Path(tempfile.mkdtemp())
-    test_folder = working_space / "tests"
+    test_folder = working_space / 'tests'
     test_folder.mkdir()
     test_files = [x.absolute() for x in test_files]
     required_odoo_modules = []
@@ -63,50 +58,37 @@ def _make_archive(verbose, test_files, root_dir):
     try:
         for file in test_files:
             shutil.copy(file, test_folder / file.name)
-            required_odoo_modules += list(
-                _get_required_odoo_modules_from_robot_file(file.read_text())
-            )
+            required_odoo_modules += list(_get_required_odoo_modules_from_robot_file(
+                file.read_text()))
 
             # refactor after code review to remove consts
             for subdir in file.parent.glob("*"):
                 if subdir.is_dir():
-                    if subdir.name in ["data"]:
+                    if subdir.name in ['data']:
                         (test_folder / subdir.name).mkdir(exist_ok=True)
-                        subprocess.call(
-                            [
-                                "rsync",
-                                str(subdir) + "/",
-                                str(test_folder / subdir.name) + "/",
-                                "-ar",
-                            ]
-                        )
+                        subprocess.call(["rsync",
+                            str(subdir) + "/",
+                            str(test_folder / subdir.name) + "/",
+                            '-ar',
+                        ])
 
-        required_odoo_modules += list(
-            collect_all(root_dir, "library", "*.py", test_folder)
-        )
-        required_odoo_modules += list(
-            collect_all(root_dir, "keywords", "*.robot", test_folder)
-        )
+        required_odoo_modules += list(collect_all(root_dir, 'library', '*.py', test_folder))
+        required_odoo_modules += list(collect_all(root_dir, 'keywords', '*.robot', test_folder))
 
         if verbose:
             click.secho("Archive content:")
             for file in test_folder.glob("**/*"):
-                click.secho(f"\t\t{file.relative_to(test_folder)}", fg="grey")
+                click.secho(f"\t\t{file.relative_to(test_folder)}", fg='grey')
 
         try:
             zip_folder = Path(tempfile.mkdtemp())
             os.chdir(test_folder)
-            archive = Path(
-                shutil.make_archive(
-                    "tests",
-                    "zip",
-                    root_dir=test_folder.parent,
-                    base_dir=test_folder.name,
-                )
-            )
-            return required_odoo_modules, base64.encodebytes(
-                archive.read_bytes()
-            ).decode("utf-8")
+            archive = Path(shutil.make_archive(
+                'tests', 'zip',
+                root_dir=test_folder.parent,
+                base_dir=test_folder.name
+            ))
+            return required_odoo_modules, base64.encodebytes(archive.read_bytes()).decode('utf-8')
 
         finally:
             shutil.rmtree(zip_folder)
