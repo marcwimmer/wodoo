0e9bea432cada196c89b3fecd0a3e00ebe441def
Marc Wimmer <marc@itewimmer.de>
2022-07-09 13:00:52 +0200

    correctsetup
    correctsetup

--- setup.py
+++ setup.py
@@ -21,45 +21,48 @@ import subprocess
 import inspect
 import os
 
-# HACK to ignore wheel building from pip and just to source distribution
-if 'bdist_wheel' in sys.argv:
-    sys.exit(0)
-
-current_dir = Path(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))))
+current_dir = Path(
+    os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+)
 setup_cfg = read_configuration("setup.cfg")
-metadata = setup_cfg['metadata']
-NAME = metadata['name']
+metadata = setup_cfg["metadata"]
+NAME = metadata["name"]
 
-REQUIRED = list(filter(bool, (current_dir / metadata['name'] / 'requirements.txt').read_text().split("\n")))
+REQUIRED = list(
+    filter(
+        bool,
+        (current_dir / metadata["name"] / "requirements.txt").read_text().split("\n"),
+    )
+)
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 try:
-    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
+    with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
 except FileNotFoundError:
-    long_description = metadata['DESCRIPTION']
+    long_description = metadata["DESCRIPTION"]
 
 # Load the package's __version__.py module as a dictionary.
 about = {}
-if not metadata['version']:
-    project_slug = metadata['name'].lower().replace("-", "_").replace(" ", "_")
-    with open(os.path.join(here, project_slug, '__version__.py')) as f:
+if not metadata["version"]:
+    project_slug = metadata["name"].lower().replace("-", "_").replace(" ", "_")
+    with open(os.path.join(here, project_slug, "__version__.py")) as f:
         exec(f.read(), about)
 else:
-    about['__version__'] = metadata['version']
+    about["__version__"] = metadata["version"]
 
 
 class UploadCommand(Command):
     """Support setup.py upload."""
 
-    description = 'Build and publish the package.'
+    description = "Build and publish the package."
     user_options = []
 
     @staticmethod
     def status(s):
         """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
+        print("\033[1m{0}\033[0m".format(s))
 
     def initialize_options(self):
         pass
@@ -68,22 +71,22 @@ class UploadCommand(Command):
         pass
 
     def clear_builds(self):
-        for path in ['dist', 'build', NAME.replace("-", "_") + ".egg-info"]:
+        for path in ["dist", "build", NAME.replace("-", "_") + ".egg-info"]:
             try:
-                self.status(f'Removing previous builds from {path}')
+                self.status(f"Removing previous builds from {path}")
                 rmtree(os.path.join(here, path))
             except OSError:
                 pass
 
     def inc_version(self):
-        file = Path('setup.cfg')
+        file = Path("setup.cfg")
         lines = file.read_text()
-        find = re.findall(r'version = (.*)', lines)
-        old_version = 'version = ' + find[-1]
-        version = list(map(int, find[-1].split('.')))
+        find = re.findall(r"version = (.*)", lines)
+        old_version = "version = " + find[-1]
+        version = list(map(int, find[-1].split(".")))
         version[-1] += 1
-        version_string = '.'.join(map(str, version))
-        new_version = 'version = ' + version_string
+        version_string = ".".join(map(str, version))
+        new_version = "version = " + version_string
         lines = lines.replace(old_version, new_version)
         file.write_text(lines)
         return version_string
@@ -92,19 +95,20 @@ class UploadCommand(Command):
         self.clear_builds()
 
         # increase version
-        about['__version__'] = self.inc_version()
+        about["__version__"] = self.inc_version()
 
-        self.status('Building Source and Wheel (universal) distribution…')
+        self.status("Building Source and Wheel (universal) distribution…")
         subprocess.check_call([sys.executable, "setup.py", "sdist"])
         subprocess.check_call(["git", "add", "."])
-        subprocess.check_call(["git", "commit", "-am", f"upload {about['__version__']}"])
+        subprocess.check_call(
+            ["git", "commit", "-am", f"upload {about['__version__']}"]
+        )
 
-        self.status('Uploading the package to PyPI via Twine…')
-        env = json.loads(Path(
-            os.path.expanduser("~/.pypi_access")).read_text())
+        self.status("Uploading the package to PyPI via Twine…")
+        env = json.loads(Path(os.path.expanduser("~/.pypi_access")).read_text())
         subprocess.check_call(["/usr/local/bin/twine", "upload", "dist/*"], env=env)
 
-        self.status('Pushing git tags…')
+        self.status("Pushing git tags…")
         subprocess.check_call(["git", "tag", f"v{about['__version__']}"])
         subprocess.check_call(["git", "push", "--tags"])
         subprocess.check_call(["git", "push"])
@@ -113,52 +117,34 @@ class UploadCommand(Command):
 
         sys.exit()
 
-class InstallCommand(install):
-    """Post-installation for installation mode."""
-    def run(self):
-        install.run(self)
-        self.setup_click_autocompletion()
-
-    def setup_click_autocompletion(self):
-        for console_script in setup_cfg['options']['entry_points']['console_scripts']:
-            console_call = console_script.split("=")[0].strip()
-
-            # if click completion helper is fresh installed and not available now
-            subprocess.run([sys.executable, "-mpip", "install", "click-completion-helper"])
-            subprocess.run([
-                "click-completion-helper",
-                "setup",
-                console_call,
-            ])
-
 
 def get_data_files():
     data_files = []
-    for i, file in enumerate((current_dir / metadata['name']).rglob("*")):
+    for i, file in enumerate((current_dir / metadata["name"]).rglob("*")):
         if not file.is_file():
             continue
-        if any(file.name.endswith(x) for x in ['.pyc', '.py']):
+        if any(file.name.endswith(x) for x in [".pyc", ".py"]):
             continue
-        if file.name.startswith('.'):
+        if file.name.startswith("."):
             continue
         path = str(file.relative_to(current_dir))
         data_files.append((str(path), [path]))
 
     return data_files
 
+
 # Where the magic happens:
 setup(
-        version=about['__version__'],
-        long_description=long_description,
-        long_description_content_type='text/markdown',
-        # If your package is a single module, use this instead of 'packages':
-        #py_modules=['prlsnapshotter'],
-        data_files=get_data_files(),
-        install_requires=REQUIRED,
-        packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
-        include_package_data = True,
-        cmdclass={
-            'upload': UploadCommand,
-            'install': InstallCommand,
-        },
+    version=about["__version__"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # If your package is a single module, use this instead of 'packages':
+    # py_modules=['prlsnapshotter'],
+    data_files=get_data_files(),
+    install_requires=REQUIRED,
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    include_package_data=True,
+    cmdclass={
+        "upload": UploadCommand,
+    },
 )
--- wodoo/__init__.py
+++ wodoo/__init__.py
@@ -13,6 +13,7 @@ from .init_functions import load_dynamic_modules
 from .init_functions import _get_customs_root
 
 from .click_global_commands import GlobalCommands
+
 try:
     import click
     from .lib_clickhelpers import AliasedGroup
@@ -20,24 +21,29 @@ except ImportError:
     click = None
 from .tools import _file2env
 
-from . import module_tools # NOQA
+from . import module_tools  # NOQA
 from . import odoo_config  # NOQA
+
 SCRIPT_DIRECTORY = Path(inspect.getfile(inspect.currentframe())).absolute().parent
 
 Commands = GlobalCommands()
 
-os.environ['HOST_HOME'] = os.getenv("HOME", "")
-os.environ['ODOO_HOME'] = str(SCRIPT_DIRECTORY)
+os.environ["HOST_HOME"] = os.getenv("HOME", "")
+os.environ["ODOO_HOME"] = str(SCRIPT_DIRECTORY)
 
-class NoProjectNameException(Exception): pass
+
+class NoProjectNameException(Exception):
+    pass
+
 
 def _get_default_project_name(restrict):
     def _get_project_name_from_file(path):
         if not path.exists():
             return
-        pj = [x for x in path.read_text().split("\n") if 'PROJECT_NAME' in x]
+        pj = [x for x in path.read_text().split("\n") if "PROJECT_NAME" in x]
         if pj:
             return pj[0].split("=")[-1].strip()
+
     if restrict:
         paths = restrict
     else:
@@ -55,23 +61,42 @@ def _get_default_project_name(restrict):
             return root.name
     raise NoProjectNameException("No default project name could be determined.")
 
+
 pass_config = click.make_pass_decorator(Config, ensure=True)
 
 
 @click.group(cls=AliasedGroup)
 @click.option("-f", "--force", is_flag=True)
 @click.option("-v", "--verbose", is_flag=True)
-@click.option("-xs", '--restrict-setting', multiple=True, help="Several parameters; limit to special configuration files settings and docker-compose files. All other configuration files will be ignored.")
-@click.option("-xd", '--restrict-docker-compose', multiple=True, help="Several parameters; limit to special configuration files settings and docker-compose files. All other configuration files will be ignored.")
-@click.option("-p", '--project-name', help="Set Project-Name")
+@click.option(
+    "-xs",
+    "--restrict-setting",
+    multiple=True,
+    help="Several parameters; limit to special configuration files settings and docker-compose files. All other configuration files will be ignored.",
+)
+@click.option(
+    "-xd",
+    "--restrict-docker-compose",
+    multiple=True,
+    help="Several parameters; limit to special configuration files settings and docker-compose files. All other configuration files will be ignored.",
+)
+@click.option("-p", "--project-name", help="Set Project-Name")
 @click.option("--chdir", help="Set Working Directory")
 @pass_config
-def cli(config, force, verbose, project_name, restrict_setting, restrict_docker_compose, chdir):
+def cli(
+    config,
+    force,
+    verbose,
+    project_name,
+    restrict_setting,
+    restrict_docker_compose,
+    chdir,
+):
     config.force = force
     config.verbose = verbose
     config.restrict = {}
     if verbose:
-        os.environ['WODOO_VERBOSE'] = '1'
+        os.environ["WODOO_VERBOSE"] = "1"
     if chdir:
         os.chdir(chdir)
         config.WORKING_DIR = chdir
@@ -85,68 +110,103 @@ def cli(config, force, verbose, project_name, restrict_setting, restrict_docker_
         for test in files:
             test = Path(test)
             if not test.exists():
-                click.secho(f"Not found: {test}", fg='red')
+                click.secho(f"Not found: {test}", fg="red")
                 sys.exit(-1)
             yield test.absolute()
 
-    config.restrict['settings'] = list(_collect_files(restrict_setting))
-    config.restrict['docker-compose'] = list(_collect_files(restrict_docker_compose))
+    config.restrict["settings"] = list(_collect_files(restrict_setting))
+    config.restrict["docker-compose"] = list(_collect_files(restrict_docker_compose))
 
     if project_name:
         config.project_name = project_name
     else:
         try:
-            config.project_name = _get_default_project_name(config.restrict['settings'])
+            config.project_name = _get_default_project_name(config.restrict["settings"])
         except NoProjectNameException:
             config.project_name = ""
-    os.environ['project_name'] = config.project_name
-    os.environ['docker_compose'] = str(config.files.get('docker_compose')) or ''
-    os.environ['CUSTOMS_DIR'] = str(config.WORKING_DIR)
+    os.environ["project_name"] = config.project_name
+    os.environ["docker_compose"] = str(config.files.get("docker_compose")) or ""
+    os.environ["CUSTOMS_DIR"] = str(config.WORKING_DIR)
 
-    load_dynamic_modules(config.dirs['images'])
+    load_dynamic_modules(config.dirs["images"])
 
     if config.verbose:
-        print(config.files['docker_compose'])
+        print(config.files["docker_compose"])
+
+
+@cli.command
+@click.option(
+    "-x",
+    "--execute",
+    is_flag=True,
+    help=("Execute the script to insert completion into users rc-file."),
+)
+def completion(execute):
+    shell = os.environ["SHELL"].split("/")[-1]
+    rc_file = os.path.expanduser(f"~/.{shell}rc")
+    line = f'eval "$(_ODOO_COMPLETE={shell}_source odoo)"'
+    if execute:
+        content = rc_file.read_text()
+        if not list(
+            filter(
+                lambda x: line in x and not x.strip().startswith("#"),
+                content.splitlines(),
+            )
+        ):
+            content += f"\n{line}"
+            click.secho(
+                f"Inserted successfully\n{line}"
+                "\n\nPlease restart you shell."
+                )
+        else:
+            click.secho("Nothing done - already existed.")
+
+
+    click.secho("\n\n" f"Insert into {rc_file}\n\n" f"echo 'line' >> {rc_file}" "\n\n")
+
 
 from . import lib_clickhelpers  # NOQA
-from . import lib_composer # NOQA
-from . import lib_backup # NOQA
-from . import lib_control # NOQA
-from . import lib_db # NOQA
-from . import lib_db_snapshots # NOQA
-from . import lib_lang # NOQA
-from . import lib_module # NOQA
-from . import lib_setup # NOQA
-from . import lib_src # NOQA
-from . import lib_docker_registry # NOQA
-from . import lib_venv # NOQA
-from . import lib_turnintodev # NOQA
+from . import lib_composer  # NOQA
+from . import lib_backup  # NOQA
+from . import lib_control  # NOQA
+from . import lib_db  # NOQA
+from . import lib_db_snapshots  # NOQA
+from . import lib_lang  # NOQA
+from . import lib_module  # NOQA
+from . import lib_setup  # NOQA
+from . import lib_src  # NOQA
+from . import lib_docker_registry  # NOQA
+from . import lib_venv  # NOQA
+from . import lib_turnintodev  # NOQA
 
 # import container specific commands
-from .tools import abort # NOQA
-from .tools import __dcrun # NOQA
-from .tools import __dc # NOQA
+from .tools import abort  # NOQA
+from .tools import __dcrun  # NOQA
+from .tools import __dc  # NOQA
+
 
 @cli.command()
 def install_completion():
     def setup_for_shell_generic(shell, shell_call):
         path = Path(f"/etc/{shell}_completion.d")
         NAME = shell_call.upper().replace("-", "_")
-        completion = subprocess.check_output([sys.argv[0]], env={f"_{NAME}_COMPLETE": f"{shell}_source"}, shell=True)
+        completion = subprocess.check_output(
+            [sys.argv[0]], env={f"_{NAME}_COMPLETE": f"{shell}_source"}, shell=True
+        )
         if path.exists():
             if os.access(path, os.W_OK):
                 (path / shell_call).write_bytes(completion)
                 return
 
         if not (path / shell_call).exists():
-            rc = Path(os.path.expanduser("~")) / f'.{shell}rc'
+            rc = Path(os.path.expanduser("~")) / f".{shell}rc"
             if not rc.exists():
                 return
-            complete_file = rc.parent / f'.{shell_call}-completion.sh'
+            complete_file = rc.parent / f".{shell_call}-completion.sh"
             complete_file.write_bytes(completion)
             if complete_file.name not in rc.read_text():
                 content = rc.read_text()
-                content += '\nsource ~/' + complete_file.name
+                content += "\nsource ~/" + complete_file.name
                 rc.write_text(content)
 
     name = Path(sys.argv[0]).name
