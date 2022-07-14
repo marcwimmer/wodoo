7ec188b92004d63c669d227b7e9d3919fb2f6cf1
odoo <odoo>
2022-07-14 15:31:22 +0200

    clean

--- wodoo/lib_module.py
+++ wodoo/lib_module.py
@@ -1245,6 +1245,8 @@ def make_dir_hashes(ctx, config, on_need):
     file_dirhashes = Path(customs_dir) / FILE_DIRHASHES
     if on_need and file_dirhashes.exists():
         return
+    
+    subprocess.check_call(["git", "clean", "-xdff"], cwd=customs_dir)
     hashes = subprocess.check_output(
         ["sha1deep", "-r", "-l", "-j", "5", customs_dir], encoding="utf8"
     ).strip()
