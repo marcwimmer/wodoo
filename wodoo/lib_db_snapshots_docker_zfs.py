ba44c522daae5c097686c80c306218e40609d87a
Marc Wimmer <marc@zebroo.de>
2024-04-11 18:05:08 +0200

    main: Fixed

--- wodoo/lib_db_snapshots_docker_zfs.py
+++ wodoo/lib_db_snapshots_docker_zfs.py
@@ -363,7 +363,13 @@ def remove_volume(config):
     zfs = search_env_path("zfs")
     umount = search_env_path("umount")
     for path in _get_possible_snapshot_paths(config):
-        subprocess.check_call(["sudo", zfs, "set", "canmount=noauto", path])
+        try:
+            subprocess.check_call(["sudo", zfs, "set", "canmount=noauto", path])
+        except:
+            click.secho(
+                "Failed to execute canmount=noauto, but perhaps not a problem. Trying to continue.",
+                fg="yellow",
+            )
         try:
             subprocess.check_call(["sudo", umount, path])
         except:
