bb5ce454429b807a7733375cc61ae77ed3deb044
Marc Wimmer <marc@zebroo.de>
2025-07-02 08:34:25 +0200

    comment

--- wodoo/lib_bakery.py
+++ wodoo/lib_bakery.py
@@ -110,6 +110,7 @@ def bake(ctx, config, params, no_update_images):
         * On port 8072 either longpolling or websocket is running
         * the path /longpolling  or /websocket (since V16) must point to that service
         * either setup your own nginx or use the proxy container
+        * TIP: when using the proxy container set PROXY_ODOO_HOST to the full qualified container name of odoo port 8069
         """,
         "3. a new database is not initialized automatically - just take an existing dump or make a new dump locally",
         """
